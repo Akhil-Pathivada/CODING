9390d617d236299603327a649be29394694dc117
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-05-15 15:25:35 +0530

    added on 15-05-2021

--- CODING/3. Linked_List/Delete_last_occurence.c
+++ CODING/3. Linked_List/Delete_last_occurence.c
@@ -8,65 +8,58 @@ Space Complexity : O(1)
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node 
-{
+struct node {
+
 	int data;
 	struct node *next;
 };
 
-struct node *newNode(int data)
-{
+struct node *newNode(int data) {
+
 	struct node *nn = (struct node*)malloc(sizeof(struct node));
-	
 	nn->data = data;
-	
 	nn->next = NULL;
 	
 	return nn;
 }
 
-void removeLastOccurence(struct node **head_addr, int key)
-{
+void removeLastOccurence(struct node **head_addr, int key) {
+
 	struct node *head = *head_addr;
-	
 	struct node *curr = head;
-	
 	struct node *temp, *target = NULL;
 	
-	for( ; curr; curr=curr->next)
-	{
-		if(curr->data == key)
+	for( ; curr; curr = curr->next) {
+
+		if(curr->data == key) {
 			target = curr;
+		}
 	}
 	
-	if(!target)
-		return;
+	if(!target) {
+		return; 
+	}
 	
-	if(target == head)
-	{
+	if(target == head) {
+
 		*head_addr = (*head_addr)->next;
-		
 		return;
 	}
 	
-	if(target->next)
-	{
+	if(target->next) {
+
 		temp = target->next;
-		
 		target->data = temp->data;
-		
 		target->next = temp->next;
-		
 		free(temp);
 		
 		return;
 	}
-	
 	free(target);
 }
 
-void main()
-{
+int main() {
+
 	struct node *head=newNode(1);
 	head->next = newNode(2);
 	head->next->next = newNode(4);
@@ -79,6 +72,8 @@ void main()
 	removeLastOccurence(&head,key);
 	
 	// Printing the list
-	for( ; head; head=head->next)
-		printf(" %d->",head->data);
+	for( ; head; head = head->next) {
+		printf(" %d->", head->data);
+		
+	}
 }
--- CODING/3. Linked_List/Rotate_linked_list.c
+++ CODING/3. Linked_List/Rotate_linked_list.c
@@ -3,71 +3,76 @@
  Time Complexity :: O(n)
  Space Complexity :: O(1)
 */
+
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
+
 	int data;
 	struct node *next;
-}Node;
+} Node;
 
-Node *head=NULL,*head2=NULL;
+Node *head = NULL, *head2 = NULL;
 
-struct node *newNode(int val)
-{
-	struct node *nn=(struct node*)malloc(sizeof(struct node));
-	nn->data=val;
-	nn->next=NULL;
+struct node *newNode(int val) {
+
+	struct node *nn = (struct node*)malloc(sizeof(struct node));
+	nn->data = val;
+	nn->next = NULL;
 
 	return nn;
 }
 
-void Insert(int val)
-{
-	Node *nn=(Node*)malloc(sizeof(Node));
-	nn->data=val;
-	nn->next=head;
-	head=nn;
+void Insert(int val) {
+
+	Node *nn = (Node*)malloc(sizeof(Node));
+	nn->data = val;
+	nn->next = head;
+	head = nn;
 }
 
-void Display(Node *cur)
-{
-	for( ;cur; cur=cur->next)
-		printf(" %d->",cur->data);
+void Display(Node *curr) {
 
+	for( ; curr; curr = curr->next) {
+		printf(" %d->",curr->data);
+	}
 	printf("\n");
 }
 
-void rotateList(Node **main_head,int d)
-{
-	Node *temp_head,*lastNode,*temp_last;
-	temp_last=*main_head;
+void rotateList(Node **main_head, int d) {
 
-	for(int i=1;i<d;i++) // O(d)
-		temp_last=temp_last->next;
+	Node *temp_head, *lastNode;
+	Node *temp_last = *main_head;
 
-	temp_head=temp_last->next;
-	for(lastNode=temp_head; lastNode->next ; lastNode=lastNode->next); // O(n-d)
+	for(int i = 1; i < d; ++i) { // O(d) 
+		temp_last = temp_last->next; 
+	}
+	temp_head = temp_last->next;
+	lastNode = temp_head;
 
-	lastNode->next=*main_head;
-	temp_last->next=NULL;
-	*main_head=temp_head;
+	while(lastNode->next) { // O(n-d)
+
+		lastNode = lastNode->next;
+	}
+
+	lastNode->next = *main_head;
+	temp_last->next = NULL;
+	*main_head = temp_head;
 }
 
-int main()
-{
-	struct node *head=newNode(1);
-	head->next=newNode(2);
-	head->next->next=newNode(3);
-	head->next->next->next=newNode(4);
-	head->next->next->next->next=newNode(5);
-	head->next->next->next->next->next=newNode(6);
+int main() {
+
+	struct node *head = newNode(1);
+	head->next = newNode(2);
+	head->next->next = newNode(3);
+	head->next->next->next = newNode(4);
+	head->next->next->next->next = newNode(5);
+	head->next->next->next->next->next = newNode(6);
 		
 	Display(head);
-	int d=2;
-	rotateList(&head,d);
+	int d = 2;
+	rotateList(&head, d);
 	Display(head);
 } 
-
 
