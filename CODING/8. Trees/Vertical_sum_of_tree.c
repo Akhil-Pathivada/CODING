c42bb3dd355c7d24356d08490aa45106c19ea2a7
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-05-19 17:49:19 +0530

    added on 19-05-2021

--- CODING/8. Trees/Vertical_sum_of_tree.c
+++ CODING/8. Trees/Vertical_sum_of_tree.c
@@ -8,22 +8,21 @@ Space Complexity :: O(n)+O(n)......(Recursion + DLL)
 #include<stdlib.h>
 #include<math.h>
 
-struct node
-{
+struct node {
+
 	int data;
 	struct node *left,*right;
 };
 
-struct DLL
-{
+struct DLL {
+
 	int data;
 	struct DLL *prev, *link, *next;
 };
 
-void makeLink(struct DLL *node1, struct DLL *node2) //O(1)
-{
+void makeLink(struct DLL *node1, struct DLL *node2) { //O(1) 
+
 	struct DLL *temp1 = node1->prev;
-	
 	struct DLL *temp2 = node1->next;
 	
 	temp1->next = node2;
@@ -34,80 +33,72 @@ void makeLink(struct DLL *node1, struct DLL *node2) //O(1)
 	node2->link = node1;
 }
 
-struct node *newNode(int val)
-{
+struct node *newNode(int val) {
+
 	struct node *nn = (struct node*)malloc(sizeof(struct node));
-	
 	nn->data = val;
-	
 	nn->left = nn->right = NULL;
 	
 	return nn;
 }
 
-void getMinMax(struct node *root,struct DLL *head) //O(n)
-{
-	if(root)
-	{
+void getMinMax(struct node *root,struct DLL *head) { //O(n) 
+
+	if(root) {
+
 		head->data = root->data;
 		
 		struct DLL *leftNode, *rightNode;
 		
-		if(root->left)
-		{
+		if(root->left) {
+
 			leftNode = (struct DLL*)malloc(sizeof(struct DLL));
 			
 			struct DLL *temp = head->prev;
-			
 			head->prev = leftNode;
-			
 			leftNode->next = head;
 			
 			if(temp)
 				makeLink(temp,leftNode);
 		}
 		
-		if(root->right)
-		{
+		if(root->right) {
+
 			rightNode = (struct DLL*)malloc(sizeof(struct DLL));
 			
 			struct DLL *temp = head->next;
-			
 			head->next = rightNode;
-			
 			rightNode->prev = head;
 			
 			if(temp)
 				makeLink(temp,rightNode);
 		}
-		
 		getMinMax(root->left,leftNode);
-		
 		getMinMax(root->right,rightNode);
 	}
 }
-void printVerticalOrder(struct DLL *head) // printing the DLL Table...... O(n)
-{
-	for( ; head; head=head->next)
-	{
-		int sum=0;
+
+void printVerticalOrder(struct DLL *head) { // printing the DLL Table...... O(n)
+
+	for( ; head; head = head->next) {
+
+		int sum = 0;
+		struct DLL *curr = head;
 		
-		struct DLL *curr=head
-		
-		for( ;curr; curr=curr->link)
+		for( ; curr; curr = curr->link)
 			sum += curr->data;
 			
-		printf(" %d,",sum);
+		printf(" %d,", sum);
 	}
 }
 
-int sizeOfTree(struct node *root)
-{
+int sizeOfTree(struct node *root) {
+
 	return root? 1 + sizeOfTree(root->left) + sizeOfTree(root->right) : 0;
 }
 
-void main()
-{
+void main() {
+
 	struct node *root = newNode(10);
 	root->left = newNode(20);
 	root->right = newNode(30);
@@ -127,9 +118,9 @@ void main()
 	
 	struct DLL *head = (struct DLL*)malloc(sizeof(struct DLL));
 	
-	getMinMax(root,head);
+	getMinMax(root, head);
 	
-	for( ;head->prev;head=head->prev); // traversing to the Head Node
+	for( ; head->prev; head = head->prev); // traversing to the Head Node
 	
 	printVerticalOrder(head);
 }
