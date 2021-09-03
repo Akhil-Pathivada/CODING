32377ebb3f09647de76fb43fbbe704db39de1553
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-03 08:22:59 +0530

    modified@Fri  3 Sep 2021 08:22:59 IST

--- CODING/3. Linked_List/ReOrder_linked_list.java
+++ CODING/3. Linked_List/ReOrder_linked_list.java
@@ -6,98 +6,80 @@ Time Complexity : O(n)
 Space Complexity : O(1)
 */
 
-class Node
-{
+class Node {
+
 	int data;
 	Node next;
 	
-	Node(int item)
-	{
+	Node(int item) {
+
 		data = item;
-		
 		next = null;
 	}
 }
 
-class LinkedList
-{	
-	Node mergeAlternates( Node head1, Node head2)
-	{
+class LinkedList {	
+
+	Node mergeAlternates( Node head1, Node head2) {
+
 		Node dummyHead = new Node(0);
-		
 		Node current = dummyHead;
 		
-		while(head1 != null && head2 != null)
-		{
+		while(head1 != null && head2 != null) {
+
 			current.next = head1;
-			
 			head1 = head1.next;
-			
 			current = current.next;
-			
 			current.next = head2;
-			
 			head2 = head2.next;
-			
 			current = current.next;
 		}
-		
 		current.next = head1;
 		
 		return dummyHead.next;
-		
 	}
 	
-	Node getMidNode(Node head)
-	{
+	Node getMidNode(Node head) {
+
 		Node slowPtr = head;
-		
 		Node fastPtr = head.next;
 		
-		while(fastPtr != null && fastPtr.next != null)
-		{
+		while(fastPtr != null && fastPtr.next != null) {
+
 			slowPtr = slowPtr.next;
-			
 			fastPtr = fastPtr.next.next;
 		}
-		
 		Node next = slowPtr.next;
-		
 		slowPtr.next = null;
 		
 		return next;
 			
 	}
 	
-	Node Reverse(Node currPtr)
-	{	
+	Node Reverse(Node currPtr) {	
+
 		Node prevPtr = null;
 		
-		while(currPtr != null)
-		{
+		while(currPtr != null) {
+
 			Node nextPtr = currPtr.next;
-			
 			currPtr.next = prevPtr;
-			
 			prevPtr = currPtr;
-			
 			currPtr = nextPtr;
 		}
-		
 		return prevPtr;
 	}
 	
-	Node reOrderList(Node head)
-	{
+	Node reOrderList(Node head) {
+
 		Node mid = getMidNode(head);
-		
 		mid = Reverse(mid);
 			
 		return mergeAlternates(head, mid);
 	}
 	
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
+
 		LinkedList ob = new LinkedList();
 		
 		Node head = new Node(1);
@@ -109,7 +91,7 @@ class LinkedList
 	
 		head = ob.reOrderList(head);
 		
-		for( ; head != null; head=head.next)
+		for( ; head != null; head = head.next)
 			System.out.print(head.data + " ");
 	}
 }
