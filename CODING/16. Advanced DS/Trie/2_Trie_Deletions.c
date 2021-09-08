bab99f9fb57ab6cd45eb5046f51c8c5f4e00d49e
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-08 22:16:04 +0530

    modified@Wed  8 Sep 2021 22:16:03 IST

--- CODING/16. Advanced DS/Trie/2_Trie_Deletions.c
+++ CODING/16. Advanced DS/Trie/2_Trie_Deletions.c
@@ -8,55 +8,50 @@
 
 #define CHAR_TO_INDEX(ch) ((int)ch - (int)'a') 
 
-struct node
-{
+struct node {
+
 	struct node *alphabet[SIZE];
 	bool isEnd;
 };
 
-struct node *newNode()
-{
+struct node *newNode() {
+
 	struct node *nn = (struct node*)malloc(sizeof(struct node));
 	
-	for(int i=0; i<SIZE; ++i)
+	for(int i = 0; i < SIZE; ++i) {
+
 		nn->alphabet[i] = NULL;
-	
+	}
 	nn->isEnd = false;
 	
 	return nn;
 }
 
-void insertKeys( struct node *root, char *str)
-{
+void insertKeys(struct node *root, char *str) {
+
 	int length = strlen(str);
-	
-	int level = 0;
-	
-	int index;
-	
 	struct node *ptr = root;
 	
-	for(int i=0; i<length; ++i)
-	{
-		index = CHAR_TO_INDEX(str[i]);
+	for(int i = 0; i < length; ++i) {
+
+		int index = CHAR_TO_INDEX(str[i]);
 		
-		if(!ptr->alphabet[index])
-			ptr->alphabet[index] = newNode();
-		
+		if(!ptr->alphabet[index]) {
+
+			ptr->alphabet[index] = newNode(); 
+		}
 		ptr = ptr->alphabet[index];
 	}
-	
 	ptr->isEnd = true;
 }
 
-bool search( struct node *root, char *str)
-{
+bool search(struct node *root, char *str) {
+
 	int n = strlen(str);
-	
 	struct node *ptr = root;	
 	
-	for(int i=0; i<n; ++i)
-	{
+	for(int i = 0; i < n; ++i) {
+
 		int index = CHAR_TO_INDEX(str[i]);
 		
 		if(!ptr->alphabet[index])
@@ -64,65 +59,69 @@ bool search( struct node *root, char *str)
 		
 		ptr = ptr->alphabet[index];
 	}
-	
 	return ptr->isEnd;	
 }
 
-bool isEmpty( struct node *root)
-{
-	for(int i=0; i<SIZE; ++i)
-	{
-		if(root->alphabet[i])
+bool isEmpty(struct node *root) {
+
+	for(int i = 0; i < SIZE; ++i) {
+
+		if(root->alphabet[i]) {
+
 			return false;
+			
+		}
 	}
 	return true;
 }
 
-struct node *Delete( struct node *root, char *str, int i, int n)
-{
-	if(!root)
-		return NULL;
+struct node *Delete(struct node *root, char *str, int i, int n) {
+
+	if(!root) {
+
+		return NULL; 
+	}
 	
-	if(i == n)
-	{
-		if(root->isEnd)
+	if(i == n) {
+
+		if(root->isEnd) {
+
 			root->isEnd = false;
+		}
 		
-		if(isEmpty(root))
-		{
+		if(isEmpty(root)) {
+
 			free(root);
-			
 			root = NULL;	
 		}
-			
 		return root;	
 	}
-	
+
 	int index = CHAR_TO_INDEX(str[i]);
-	
+
 	root->alphabet[index] = Delete(root->alphabet[index], str, ++i, n);
 	
-	if(isEmpty(root) && !root->isEnd)
-	{
+	if(isEmpty(root) && !root->isEnd) {
+
 		free(root);
-		
 		root = NULL;
 	}
-	
 	return root;
 }
 
-void main()
-{
+int main() {
+
 	char keys[][8] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
 
 	struct node *root = newNode(); 
 	
-	int n = sizeof(keys)/sizeof(*keys);
+	int n = sizeof(keys) / sizeof(*keys);
 	
 	// Construct Trie
-	for(int i=0; i<n; ++i)
-		insertKeys(root, keys[i]);
+	for(int i = 0; i < n; ++i) {
+
+		insertKeys(root, keys[i]); 
+	}
 		
 	Delete(root, "answer", 0, strlen("answer"));
 
