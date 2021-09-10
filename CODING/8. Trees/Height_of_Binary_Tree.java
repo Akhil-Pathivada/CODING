764728d434dcb2ae8bbe592ed008633edb854ce3
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-10 07:35:47 +0530

    modified@Fri 10 Sep 2021 07:35:47 IST

--- CODING/8. Trees/Height_of_Binary_Tree.java
+++ CODING/8. Trees/Height_of_Binary_Tree.java
@@ -7,61 +7,62 @@ Space Complexity : O(n)
 
 import java.util.*;
 
-class Node
-{
+class Node {
+
 	int data;
 	Node left, right;
 	
-	Node(int item)
-	{
+	Node(int item) {
+
 		data = item;
-		
 		left = right = null;
 	}
 }
 
-class BinaryTree
-{	
-	static int getHeight(Node root)
-	{
+class BinaryTree {	
+
+	static int getHeight(Node root) {
+
 		int height = 0;
-		
 		Queue<Node> queue = new LinkedList<Node>();
-		
 		queue.add(root);
 		
-		while(true)
-		{
+		while(true) {
+
 			int nodeCount = queue.size();
 			
-			if(nodeCount == 0)
-				return height;
+			if(nodeCount == 0) {
+
+				return height; 
+			}
+			++height;
 			
-			height++;
-			
-			while(nodeCount > 0)
-			{
+			while(nodeCount > 0) {
+
 				Node temp = queue.remove();
 				
-				if(temp.left != null)
-					queue.add(temp.left);
+				if(temp.left != null) {
+
+					queue.add(temp.left); 
+				}
 				
-				if(temp.right != null)
-					queue.add(temp.right);
-					
-				nodeCount--;
+				if(temp.right != null) {
+
+					queue.add(temp.right); 
+				}
+				--nodeCount;
 			}
 		}
 	}
 	
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
+
 		Node root = new Node(1);
 		root.left = new Node(2);
 		root.right = new Node(3);
 		root.left.left = new Node(4);
 		root.left.right = new Node(5);
 		
-		System.out.println(getHeight(root));
+		System.out.println("Height of Binary Tree = " + getHeight(root));
 	}
 }
