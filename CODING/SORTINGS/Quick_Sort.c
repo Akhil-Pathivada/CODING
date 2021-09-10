a2ae5a6aaa19d769acd4da25b069d351eee16e34
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-10 08:04:57 +0530

    modified@Fri 10 Sep 2021 08:04:57 IST

--- CODING/SORTINGS/Quick_Sort.c
+++ CODING/SORTINGS/Quick_Sort.c
@@ -6,71 +6,70 @@ Space Complexity : O(1)
 #include<stdio.h>
 #include<stdlib.h>
 
-void PrintArray(int *arr,int size)
-{
-	for(int i=0;i<size;i++)
+void PrintArray(int *arr, int size) {
+
+	for(int i = 0; i < size; ++i) {
+	
 		printf(" %d,",arr[i]);
+	}
 }
 
-void Swap(int *p,int *q)
-{
-	int temp=*p;
-	*p=*q;
-	*q=temp;
+void Swap(int *p, int *q) {
+
+	int temp = *p;
+	*p = *q;
+	*q = temp;
 }
 
-int Partition(int *arr,int low,int high)
-{
+int Partition(int *arr, int low, int high) {
+
 	int x = arr[high];
 
 	int i = low-1;
 
-	for(int j=low; j<high; ++j)
-	{
-		if(arr[j] <= x)
-		{
-			i++;
-			
+	for(int j = low; j < high; ++j) {
+
+		if(arr[j] <= x) {
+
+			++i;
 			Swap(&arr[i], &arr[j]);
 		}
 	}
-
 	Swap(&arr[++i], &arr[high]);
 
 	return i;
 }
 
-void quickSort(int *arr,int low,int high)
-{
-	if(low<high)
-	{
-		int pivot=Partition(arr,low,high);
+void quickSort(int *arr, int low, int high) {
 
-		quickSort(arr,low,pivot-1);
+	if(low < high) {
 
-		quickSort(arr,pivot+1,high);
+		int pivot = Partition(arr, low, high);
+
+		quickSort(arr, low, pivot - 1);
+
+		quickSort(arr, pivot + 1, high);
 	}
 }
 
-int main()
-{
-	int *arr,size;
+int main() {
 
-	printf("\nEnter size of the Array :: ");	
-	scanf("%d",&size);	
+	int *arr, size;
 
-	arr=(int*)malloc(sizeof(int)*size);
+	printf("Enter size of the Array : ");	
+	scanf("%d", &size);	
+
+	arr = (int*)malloc(sizeof(int)*size);
 	
-	printf("\nEnter the Array :: ");	
+	printf("Enter the Array : ");	
+	for(int i = 0; i < size; ++i) {
 
-	for(int i=0;i<size;i++)
-		scanf("%d",&arr[i]);
+		scanf("%d", &arr[i]);
+	}
 
-	PrintArray(arr,size);
-
-	quickSort(arr,0,size-1);
+	PrintArray(arr, size);
+	quickSort(arr, 0, size - 1);
 
 	printf("** Sorted Successfully **");
-
-	PrintArray(arr,size);
+	PrintArray(arr, size);
 }
