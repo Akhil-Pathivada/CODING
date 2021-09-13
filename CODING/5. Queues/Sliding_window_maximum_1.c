6a7ab9dc2aab3dea99da3e3b732126ee70ec6b87
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-13 08:08:24 +0530

    modified@Mon 13 Sep 2021 08:08:24 IST

--- CODING/5. Queues/Sliding_window_maximum_1.c
+++ CODING/5. Queues/Sliding_window_maximum_1.c
@@ -1,32 +1,31 @@
 /*
 Given an array and an integer K, find the maximum for each and every contiguous subarray of size k.
 
-Time Complexity : O(n*k)
-Space Complexity : O(k)
+Time Complexity : O(N * K)
+Space Complexity : O(K)
 */
 
 #include<stdio.h>
 
-void printMax( int *arr, int n, int k)
-{
-	for(int i=0; i<=n-k; ++i)
-	{
+void printMax(int *arr, int n, int k) {
+
+	for(int i = 0; i <= n-k; ++i) {
+
 		int max = arr[i];
 		
-		for(int j=i; j<i+k; ++j)
+		for(int j = i; j < i+k; ++j) {
+
 			max = arr[j] > max ? arr[j] : max;
-		
+		}
 		printf("%d ", max);
 	}
 }
 
-void main()
-{
+int main() {
+
 	int arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6};
-	
-	int n = sizeof(arr)/sizeof(*arr);
-	
+	int n = sizeof(arr) / sizeof(*arr);
 	int k = 3;
 	
-	printMax(arr,n,k);
+	printMax(arr, n, k);
 }
