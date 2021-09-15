722ec56ea9d83150fedd54fc1deaa3be4a4b90dd
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-15 19:49:11 +0530

    added@Wed 15 Sep 2021 19:49:11 IST

---
+++ CODING/6. Heaps/Maximum_product_by_selling_M_products_1.java
@@ -0,0 +1,48 @@
+/* 
+ * Given an array arr[] consisting of N positive integers, such that arr[i] represents the number of products the ith supplier has and a positive integer, M, the task is to find the maximum profit by selling M products if the profit of a particular product is the same as the number of products left of that supplier.
+ *
+ * Examples:
+ *
+ * Input: arr[] = {4, 6}, M = 4
+ * Output: 19
+ * Explanation:
+ * Below are the order of the product sell to gain the maximum profit:
+ * Product 1: Sell a product from the second supplier, then the array modifies to {4, 5} and the profit is 6.
+ * Product 2: Sell a product from the second supplier, then the array modifies to{4, 4} and the profit is 6 + 5 = 11.
+ * Product 3: Sell a product from the second supplier, then the array modifies to {4, 3} and the profit is 6 + 5 + 4 = 15.
+ * Product 4: Sell a product from the first supplier, then the array modifies to {3, 3} and the profit is 6 + 5 + 4 + 4 = 19.
+ * Therefore, the maximum profit that can be obtained by selling 4 products is 19.
+ *
+ * Time Complexity : O(M * N)
+ * Space Complexity : O(1)
+ *  */
+
+class GFG { 
+
+	private static int findMaximumProfit(int[] arr, int M, int N) {
+
+		int maxProfit = 0;
+
+		for(int i = 0; i < M; ++i) {
+
+			int j, max = 0;
+			for(j = 1; j < arr.length; ++j) {
+
+			     if(arr[j] > arr[max]) {
+			    	     max = j;			
+			     }
+			}
+			maxProfit += arr[max];
+			arr[max]--;
+		}
+		return maxProfit;
+	} 
+	public static void main(String[] args) {
+
+		int[] arr = { 4, 6 };
+		int M = 4;
+		int N = arr.length;
+
+		System.out.println("Max. Profit : " + findMaximumProfit(arr, M, N));
+	}
+}
