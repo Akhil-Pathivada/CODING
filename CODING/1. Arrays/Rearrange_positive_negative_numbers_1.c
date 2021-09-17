2799f1e14d0678bc74eaf323001f21e6466c55fa
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-17 18:56:46 +0530

    added@Fri 17 Sep 2021 18:56:46 IST

---
+++ CODING/1. Arrays/Rearrange_positive_negative_numbers_1.c
@@ -0,0 +1,48 @@
+/** 
+  * Given a binary array, sort it using one traversal and no extra space.
+  * Examples :
+  *
+  *
+  * Input : 1 0 0 1 0 1 0 1 1 1 1 1 1 0 0 1 1 0 1 0 0
+  * Output : 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1
+  * Explanation: The output is a sorted array of 0 and 1
+  *
+  * Time Complexity : O(N)
+  * Space Complexity : O(1)
+  *  */
+
+#include<stdio.h>
+
+void swap(int *a, int *b) {
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void sortBinaryArray(int arr[], int n) {
+
+	// take pivot as 1
+	int pivot = 1;
+	int j = -1;
+	for(int i = 0; i < n; ++i) {
+
+		if(arr[i] < pivot) {
+		    
+			swap(&arr[i], &arr[++j]);
+		}
+	}
+}
+
+int main() {
+
+	int arr[] = { 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0 };
+
+	int n = sizeof(arr) / sizeof(*arr);
+
+	sortBinaryArray(arr, n);
+	for (int i = 0; i < n; i++) {
+		
+		printf(" %d, ", arr[i]);
+	}
+}
