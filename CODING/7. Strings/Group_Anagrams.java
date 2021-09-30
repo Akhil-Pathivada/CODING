2a5e940241b36c039c24f13dab7aaa5fedd6576d
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-30 22:18:55 +0530

    added@Thu 30 Sep 2021 22:18:55 IST

---
+++ CODING/7. Strings/Group_Anagrams.java
@@ -0,0 +1,51 @@
+/*
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example :
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Time Complexity : O(N)
+Space Complexity : O(N)
+*/
+
+import java.util.*;
+
+class GFG {
+
+	static List<List<String>> groupAnagrams(String[] strs) {
+
+		Map<String, List<String>> map = new HashMap<>();
+
+		for(String s : strs) {
+
+			// character array
+			char[] ca = new char[26];
+
+			for(char ch : s.toCharArray()) {
+				ca[ch - 'a']++;
+			}
+			// converting character array as a string for comparison
+			String keyStr = String.valueOf(ca);
+			// adding string as key for 1st time and allocating memory to store its anagrams
+			if(!map.containsKey(keyStr)) {
+				map.put(keyStr, new ArrayList<>());
+			}
+			// adding the anagram
+			map.get(keyStr).add(s);
+		}
+		return new ArrayList<>(map.values());
+	}
+ 
+	public static void main(String[] args) {
+
+		String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+
+		System.out.println("Grouped Anagrams : " + groupAnagrams(strs));
+	}
+}
+
