dc386002dee5033fe67bf31d59e2643876a2b23e
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-09-30 14:23:21 +0530

    added@Thu 30 Sep 2021 14:23:21 IST

---
+++ CODING/Sign-in-Sign-out-Logs_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+        
+ class GFG {
+    public static void main(String[] args) {
+
+        List<String> input = new ArrayList<>() {{
+            add ("345366 89921 45");
+            add ("029323 38239 23");
+            add ("38239 345366 15");
+            add ("029323 38239 77");
+            add ("345366 38239 23");
+            add ("029323 345366 13");
+            add ("38239 38239 23");
+        }};	
+ }
