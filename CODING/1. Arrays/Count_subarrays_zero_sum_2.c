32f2913e4257a0bdaf1615b7fbf5777767b0142d
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-11 19:36:59 +0530

    modified@Mon 11 Oct 2021 19:36:59 IST

--- CODING/1. Arrays/Count_subarrays_zero_sum_2.c
+++ CODING/1. Arrays/Count_subarrays_zero_sum_2.c
@@ -6,30 +6,31 @@ Space Complexity : O(n)
 */
 #include<stdio.h>
 
-int countSubArrays(int arr[], int N)
-{
+int countSubArrays(int arr[], int N) {
+
 	int sumArr[N] , count=0;
 
 	*sumArr = arr[0];
 
-	for(int i=1; i<N; i++)
+	for(int i = 1; i < N; ++i) {
 		sumArr[i] = arr[i] + sumArr[i-1];
-
-	for(int i=0; i<N; i++)
-	{
-		for(int j=i+1; j<N; j++)
-		{
-			if(sumArr[j] == sumArr[i])
-				count++;
-		}
 	}
 
+	for(int i = 0; i < N; ++i) {
+
+		for(int j = i+1; j < N; ++j) {
+
+			if(sumArr[j] == sumArr[i]) {
+				count++;
+			}
+		}
+	}
 	return count;
 }
 
-void main()
-{
-	int arr[] = {11,10,-5,-3,-2,10,7,-1,-6};
+int main() {
+
+	int arr[] = {11, 10, -5, -3, -2, 10, 7, -1, -6};
 	
-	printf(" Total SubArrays : %d ",countSubArrays(arr,9));
+	printf(" Total SubArrays : %d ", countSubArrays(arr, 9));
 }
