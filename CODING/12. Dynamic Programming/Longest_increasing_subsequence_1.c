62dbe344709c4ce0b387e041336f2523b20408ea
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-15 08:05:03 +0530

    modified@Fri 15 Oct 2021 08:05:03 IST

--- CODING/12. Dynamic Programming/Longest_increasing_subsequence_1.c
+++ CODING/12. Dynamic Programming/Longest_increasing_subsequence_1.c
@@ -3,34 +3,38 @@ Find the length of the longest subsequence of a given sequence such that all ele
 For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}.
 
 (Naive Approach)
+Time Complexity : O((2 ^ N) * N)
 
 */
 
 #include<stdio.h>
 
-int LIS( int *arr, int n, int *maxLen)
-{
-	if(n == 1)
+int LIS(int *arr, int n, int *maxLen) {
+
+	if(n == 1) {
 		return 1;
+	}
 		
 	int res, max_ending_here = 1;
 	
-	for(int i = 1; i < n; ++i)
-	{
+	for(int i = 1; i < n; ++i) {
+
 		res = LIS(arr, i, maxLen);
 		
-		if(arr[i-1] < arr[n-1]  &&  res + 1 > max_ending_here)
+		if(arr[i-1] < arr[n-1]  &&  res + 1 > max_ending_here) {
 			max_ending_here = res + 1;
+		}
 	} 
 	
-	if(max_ending_here > *maxLen)
+	if(max_ending_here > *maxLen) {
 		*maxLen = max_ending_here;
+	}
 		
 	return max_ending_here;
 }
 
-int getLIS( int arr[], int n)
-{
+int getLIS(int arr[], int n) {
+
 	int max = 1;
 	
 	LIS(arr, n, &max);
@@ -38,11 +42,10 @@ int getLIS( int arr[], int n)
 	return max;
 }
 
-void main()
-{	
+int main() {
+
 	int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
-	
 	int n = sizeof(arr) / sizeof(*arr);
 	
-	printf(" Longest Increasing Subsequence : %d ", getLIS(arr,n));
+	printf(" Longest Increasing Subsequence : %d ", getLIS(arr, n));
 }
--- CODING/12. Dynamic Programming/Longest_increasing_subsequence_2.c
+++ CODING/12. Dynamic Programming/Longest_increasing_subsequence_2.c
@@ -11,36 +11,37 @@ Space Complexity : O(n)
 #include<stdio.h>
 #include<stdlib.h>
 
-int LIS(int *arr, int n)
-{
+int LIS(int *arr, int n) {
+
 	int lis[n];
 	
 	lis[0] = 1; 
 		
-	for(int i = 1; i < n; ++i)
-	{
+	for(int i = 1; i < n; ++i) {
+
 	    	lis[i] = 1;
 	    
-		for(int j = 0; j < i; ++j)
-		{
-			if(arr[i] > arr[j] && lis[i] < lis[j] + 1)
+		for(int j = 0; j < i; ++j) {
+
+			if(arr[i] > arr[j] && lis[i] < lis[j] + 1) {
 				lis[i] = lis[j] + 1;
+			}
 		}
 	}
 	
 	int maxLen = lis[0]; 
 	
-	for(int i = 1; i < n; ++i)
+	for(int i = 1; i < n; ++i) {
 		maxLen = lis[i] > maxLen ? lis[i] : maxLen;
+	}
 		
 	return maxLen;
 }
 
-void main()
-{	
+int main() {
+
 	int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
-	
 	int n = sizeof(arr)/sizeof(*arr);
 	
-	printf(" Longest Increasing Subsequence : %d ", LIS(arr,n));
+	printf(" Longest Increasing Subsequence : %d ", LIS(arr, n));
 }
