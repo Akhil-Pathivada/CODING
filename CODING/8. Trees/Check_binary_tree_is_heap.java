26ce77d1919f105b5bb9dd67bd608a5554d070b1
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-17 09:14:53 +0530

    modified@Sun 17 Oct 2021 09:14:53 IST

---
+++ CODING/8. Trees/Check_binary_tree_is_heap.java
@@ -0,0 +1,9 @@
+/*
+Given a binary tree, we need to check it has heap property or not, Binary tree need to fulfill the following two conditions for being a heap –
+   1.It should be a complete tree (i.e. all levels except last should be full).
+   2.Every node’s value should be greater than or equal to its child node (considering max-heap).
+Time Complexity : O(n)
+Space Complexity : O(n)
+*/
+}
+}
