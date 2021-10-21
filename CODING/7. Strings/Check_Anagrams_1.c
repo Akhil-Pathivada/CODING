456cc9a90e3516f43cc6fc32f1816917a8bf0def
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-21 22:23:28 +0530

    modified@Thu 21 Oct 2021 22:23:28 IST

--- CODING/7. Strings/Check_Anagrams_1.c
+++ CODING/7. Strings/Check_Anagrams_1.c
@@ -1,7 +1,7 @@
 /*
  Program to Check two strings are Anagrams or not....
  
- Time Complexity: O(n*logn) ..... MERGE SORT 
+ Time Complexity: O(N * log(N)) ..... MERGE SORT 
  Space Complexity : O(1)
 */
 
@@ -9,34 +9,33 @@
 #include<string.h>
 #include<stdlib.h>
 
-void Swap(char *p,char *q)
-{
-	char temp=*p;
-	*p=*q;
-	*q=temp;
-}
-int compareTo(const void *a,const void *b)
-{
-	return ( *(char*)a - *(char*)b );
+void Swap(char *p, char *q) {
+
+	char temp = *p;
+	*p = *q;
+	*q = temp;
 }
 
-int checkAnagrams(char *str1,char *str2)
-{
-	if(strlen(str1)!=strlen(str2))
+int compareTo(const void *a, const void *b) {
+
+	return (*(char*)a - *(char*)b);
+}
+
+int checkAnagrams(char *str1, char *str2) {
+
+	if(strlen(str1) != strlen(str2)) {
 		return 0;
-
-	return !strcmp(str1,str2);		
+	}
+	return !strcmp(str1, str2);		
 }
 
-void main()
-{
+int main() {
+
 	char str1[] = "ravindra";
-	
 	char str2[] = "aravindr";
 	
 	qsort(str1, strlen(str1), sizeof(*str1), compareTo);
-	
 	qsort(str2, strlen(str2), sizeof(*str2), compareTo);
 
-	printf(checkAnagrams(str1,str2) ? "** ANAGRAMS **" : "** Not ANAGRAMS **");
+	printf(checkAnagrams(str1, str2) ? "** ANAGRAMS **" : "** Not ANAGRAMS **");
 }
--- CODING/7. Strings/Check_Anagrams_2.c
+++ CODING/7. Strings/Check_Anagrams_2.c
@@ -1,7 +1,7 @@
 /*
  Program to Check two strings are Anagrams or not....
  
-Time Complexity : O(n)
+Time Complexity : O(N)
 Space Complexity : O(1)
 */
 
@@ -10,33 +10,35 @@ Space Complexity : O(1)
 #include<stdlib.h>
 #define TOTAL_CHARS 256
 
-int HashTable[TOTAL_CHARS]={0};
+int HashTable[TOTAL_CHARS] = {0};
 
-int checkAnagrams(char *str1,char *str2)
-{
-	if(strlen(str1)!=strlen(str2))
+int checkAnagrams(char *str1, char *str2) {
+
+	if(strlen(str1) != strlen(str2)) {
 		return 0;
-
-	while(*str1)
-		HashTable[*str1++]++;
-
-	while(*str2)
-		HashTable[*str2++]--;
-
-	for(int index=0 ;index<TOTAL_CHARS; index++)
-	{
-		if(HashTable[index]!=0)
-			return 0;
 	}
 
+	while(*str1) {
+		HashTable[*str1++]++;
+	}
+
+	while(*str2) {
+		HashTable[*str2++]--;
+	}
+
+	for(int index = 0 ;index < TOTAL_CHARS; ++index) {
+
+		if(HashTable[index] != 0) {
+			return 0;
+		}
+	}
 	return 1;		
 }
 
-void main()
-{
+int main() {
+
 	char str1[] = "ravindra";
-	
 	char str2[] = "aravindr";
 
-	checkAnagrams(str1,str2) ? printf("\n** ANAGRAMS **\n") : printf("\n** Not ANAGRAMS **\n");
+	printf(checkAnagrams(str1, str2) ? "** ANAGRAMS **" : "** Not ANAGRAMS **");
 }
