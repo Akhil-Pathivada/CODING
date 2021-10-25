6a0f7f4dbd92a200453ea95671384a716afb563e
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-25 18:08:43 +0530

    modified@Mon 25 Oct 2021 18:08:43 IST

--- CODING/10. Divide_Conquer/Count_inversions_1.c
+++ CODING/10. Divide_Conquer/Count_inversions_1.c
@@ -9,25 +9,26 @@ Space Complexity : O(1)
 
 #include<stdio.h>
 
-int countInversions(int *arr,int size)
-{
-	int invCount=0;
+int countInversions(int *arr, int size) {
+
+	int invCount = 0;
 	
-	for(int i=0; i<size; ++i)
-	{
-		for(int j=i+1; j<size; ++j)
-		{
-			if(arr[i] > arr[j])
+	for(int i = 0; i < size; ++i) {
+
+		for(int j = i+1; j < size; ++j) {
+
+			if(arr[i] > arr[j]) {
 				++invCount;
+			}
 		}	
 	}
 	return invCount;
 }
 
-int main()
-{
-	int arr[]={3,2,5,7,4,1,6};
-	int size=sizeof(arr)/sizeof(*arr);
+int main() {
+
+	int arr[] = {3, 2, 5, 7, 4, 1, 6};
+	int size = sizeof(arr) / sizeof(*arr);
 	
-	printf(" total inversions : %d ",countInversions(arr,size));
+	printf(" total inversions : %d ", countInversions(arr, size));
 }
