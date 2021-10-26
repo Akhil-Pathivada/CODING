b70f53dcef7c67a52510429dcd247c77c9f90188
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-10-26 07:11:32 +0530

    modified@Tue 26 Oct 2021 07:11:32 IST

--- CODING/10. Divide_Conquer/Missing_number_AP_1.c
+++ CODING/10. Divide_Conquer/Missing_number_AP_1.c
@@ -2,34 +2,35 @@
 Given an array that represents elements of arithmetic progression in order. 
 One element is missing in the progression, find the missing number. 
 
-Time Complexity : O(n)
+Time Complexity : O(N)
 Space Complexity : O(1)
 */
 
 #include<stdio.h>
 
-int findMissingNumber(int arr[],int diff,int n)
-{
-	int sum=arr[0];
+int findMissingNumber(int arr[], int diff, int n) {
+
+	int sum = arr[0];
 	
-	for(int i=1; i<n; ++i)
-	{
+	for(int i = 1; i < n; ++i) {
+
 		sum += diff;
 		
-		if(arr[i]!=sum)
+		if(arr[i] != sum) {
 			return sum;
+		}
 	}
+	return -1;
 }
 
-void main()
-{
-	int arr[]={-2,0,2,4,6,10,12,14};
-	int n=sizeof(arr)/sizeof(*arr);
+int main() {
+
+	int arr[] = {-2, 0, 2, 4, 6, 10, 12, 14};
+	int n = sizeof(arr) / sizeof(*arr);
+
+	int firstTerm = *arr;
+	int lastTerm = arr[n-1];
+	int diff = (lastTerm - firstTerm) / n;
 	
-	int firstTerm=*arr;
-	int lastTerm=arr[n-1];
-	
-	int diff = (lastTerm-firstTerm)/n;
-	
-	printf(" Missing Number : %d ",findMissingNumber(arr,diff,n));
+	printf("Missing Number : %d ", findMissingNumber(arr, diff, n));
 }
--- CODING/10. Divide_Conquer/Missing_number_AP_2.c
+++ CODING/10. Divide_Conquer/Missing_number_AP_2.c
@@ -11,41 +11,41 @@ Space Complexity : O(logN)
 #include<stdio.h>
 #include<limits.h>
 
-int getNthTerm( int a, int d, int N)
-{
-	return a + (N-1)*d;
+int getNthTerm(int a, int d, int N) {
+
+	return a + (N - 1) * d;
 }
 
-int findMissingNumber( int arr[], int diff, int start, int end)
-{
-	if(start > end)
+int findMissingNumber(int arr[], int diff, int start, int end) {
+
+	if(start > end) {
 		return INT_MAX;
+	}
 	
-	int mid = (start+end)/2;
+	int mid = (start + end) / 2;
 	
-	if(arr[mid+1]-arr[mid] != diff)
+	if(arr[mid + 1] - arr[mid] != diff) {
 		return arr[mid] + diff;
+	}
 		
-	if(arr[mid] == getNthTerm(*arr, diff, mid+1))
-		return findMissingNumber(arr, diff, mid+1, end);
+	if(arr[mid] == getNthTerm(*arr, diff, mid + 1)) {
+		return findMissingNumber(arr, diff, mid + 1, end);
+	}
 		
-	if( mid>0 && (arr[mid]-arr[mid-1] != diff))
-		return arr[mid-1]+diff;
-		
-	return findMissingNumber(arr, diff, start, mid-1);	
+	if(mid > 0 && (arr[mid] - arr[mid - 1] != diff)) {
+		return arr[mid - 1] + diff;
+	}	
+	return findMissingNumber(arr, diff, start, mid - 1);	
 }
 
-void main()
-{
+int main() {
+
 	int arr[] = {-2, 0, 2, 4, 6, 8, 12, 14};
-	
-	int n = sizeof(arr)/sizeof(*arr);
+	int n = sizeof(arr) / sizeof(*arr);
 	
 	int firstTerm = *arr;
-	
 	int lastTerm = arr[n-1];
+	int diff = (lastTerm - firstTerm) / n;
 	
-	int diff = (lastTerm-firstTerm)/n;
-	
-	printf(" Missing Number : %d ",findMissingNumber(arr,diff,0,n-1));
+	printf(" Missing Number : %d ", findMissingNumber(arr, diff, 0, n - 1));
 }
