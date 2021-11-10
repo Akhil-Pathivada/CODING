adfd46a2aa843aa7e2896c3bb2438b1a965af22c
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-11-10 18:19:05 +0530

    modified@Wed 10 Nov 2021 18:19:05 IST

--- CODING/12. Dynamic Programming/Partition_problem_1.c
+++ CODING/12. Dynamic Programming/Partition_problem_1.c
@@ -3,45 +3,48 @@ Partition problem is to determine whether a given set can be partitioned into tw
 
 (Recursive Naive Approach)
 
-Time Complexity : O(2^n)
+Time Complexity : O(2 ^ n)
 Space Complexity : O(n)
 */
 
 #include<stdio.h>
 #include<stdbool.h>
 
-bool isSubsetSum( int *arr, int sum, int n)
-{
-	if(!sum)
+bool isSubsetSum(int *arr, int sum, int n) {
+
+	if(!sum) {
 		return true;
+	}
 		
-	if(!n && sum)
+	if(!n && sum) {
 		return false;	
+	}
 	
-	if(arr[n-1] > sum)
-		return isSubsetSum(arr,sum,n-1);
+	if(arr[n - 1] > sum) {
+		return isSubsetSum(arr, sum, n - 1);
+	}
 		
-	return isSubsetSum(arr, sum-arr[n-1], n-1) || isSubsetSum(arr,sum, n-1); 
+	return isSubsetSum(arr, sum - arr[n - 1], n - 1) || isSubsetSum(arr, sum, n - 1); 
 }
 
-bool checkPartition( int *arr, int N)
-{
-	int sum=0;
+bool checkPartition(int *arr, int N) {
 
-	for(int i=0; i<N; ++i)
+	int sum = 0;
+
+	for(int i = 0; i < N; ++i) {
 		sum += arr[i];
+	}
 		
-	if(sum & 1)
+	if(sum & 1) {
 		return false;
-		
-	return isSubsetSum(arr, sum/2, N);	
+	}
+	return isSubsetSum(arr, sum / 2, N);	
 }
 
-void main()
-{
+int main() {
+
 	int arr[] = {3, 1, 5, 9, 12}; 
+	int size = sizeof(arr) / sizeof(*arr);
 	
-	int size = sizeof(arr)/sizeof(*arr);
-	
-	printf(checkPartition(arr,size) ? " YES" : " NO");
+	printf(checkPartition(arr, size) ? " YES" : " NO");
 }
--- CODING/12. Dynamic Programming/Partition_problem_2.c
+++ CODING/12. Dynamic Programming/Partition_problem_2.c
@@ -3,56 +3,60 @@ Partition problem is to determine whether a given set can be partitioned into tw
 
 (Dynamic Programming)
 
-Time Complexity : O(n*sum)
-Space Complexity : O(n*sum)
+Time Complexity : O(n * sum)
+Space Complexity : O(n * sum)
 */
 
 #include<stdio.h>
 #include<stdbool.h>
 
-bool isSubsetSum( int *arr, int sum, int n)
-{
+bool isSubsetSum(int *arr, int sum, int n) {
+
 	bool mat[n+1][sum+1];
 	
-	for(int i=0; i<=n; ++i)
+	for(int i = 0; i <= n; ++i) {
 		mat[i][0] = true;
+	}
 		
-	for(int i=1; i<=sum; ++i)
+	for(int i = 1; i <= sum; ++i) {
 		mat[0][i] = false;
+	}
 	
-	for(int i=1; i<=n; ++i)
-	{
-		for( int j=1; j<=sum; ++j)
-		{
-			if(arr[i-1] > j)
+	for(int i = 1; i <= n; ++i) {
+
+		for( int j = 1; j <= sum; ++j) {
+
+			if(arr[i-1] > j) {
 				mat[i][j] = mat[i-1][j];
-				
-			else
-				mat[i][j] = mat[i-1][j-arr[i-1]] || mat[i-1][j];
+			}
+			else {
+				mat[i][j] = mat[i-1][j - arr[i-1]] || mat[i-1][j];
+			}
 		}
 	}
 	
 	return mat[n][sum];
 }
 
-bool checkPartition( int *arr, int N)
-{
+bool checkPartition(int *arr, int N) {
+
 	int sum=0;
 
-	for(int i=0; i<N; ++i)
+	for(int i = 0; i < N; ++i) {
 		sum += arr[i];
+	}
 		
-	if(sum & 1)
+	if(sum & 1) {
 		return false;
+	}
 		
-	return isSubsetSum(arr, sum/2, N);	
+	return isSubsetSum(arr, sum / 2, N);	
 }
 
-void main()
-{
+int main() {
+
 	int arr[] = {3, 1, 5, 9, 12}; 
+	int size = sizeof(arr) / sizeof(*arr);
 	
-	int size = sizeof(arr)/sizeof(*arr);
-	
-	printf(checkPartition(arr,size) ? " YES" : " NO");
+	printf(checkPartition(arr, size) ? " YES" : " NO");
 }
