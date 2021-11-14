ed0251518e2065b4735227d370eb0e4f8af20803
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-11-14 19:19:56 +0530

    added@Sun 14 Nov 2021 19:19:56 IST

---
+++ CODING/8. Trees/TreeNode.java
@@ -0,0 +1,11 @@
+package trees;
+
+public class TreeNode {
+        
+        int data;
+        TreeNode left, right;
+        public TreeNode(int value) {
+                data = value;
+                left = right = null;
+        }
+}
