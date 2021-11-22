cbd017212148764a94bfbb45c4b6e9f110e10079
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-11-22 21:02:04 +0530

    added@Mon Nov 22 21:02:04 IST 2021

---
+++ interview/src/trees/ClosestValueInBinaryTreeI.java
@@ -0,0 +1,62 @@
+/**
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ * https://leetcode.com/problems/closest-binary-search-tree-value/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(H)
+ */
+package trees;
+
+public class ClosestValueInBinaryTreeI {
+        // tracks the min difference and closest value
+        static double minDiff;
+        static int closestValue;
+        
+        private static void findClosestValueUtil(TreeNode root, double target) {
+                // base case
+                if(root == null) {
+                        return;
+                }
+                // If target itself is present
+                if(root.data == target) {
+                        closestValue = root.data;
+                }
+                // update min_diff and closest_value by checking current node value
+                else if(Math.abs(target - root.data) < minDiff) {
+                        minDiff = Math.abs(target - root.data);
+                        closestValue = root.data;
+                }
+                /** if target is less than root data then move in
+                left subtree else in right subtree */
+                if(root.data > target) {
+                        findClosestValueUtil(root.left, target);
+                }
+                else {
+                        findClosestValueUtil(root.right, target);
+                }
+        }
+        
+        private static int findClosestValue(TreeNode root, double target) {
+                // initialize values
+                minDiff =  Double.MAX_VALUE;
+                closestValue = -1;
+                
+                findClosestValueUtil(root, target);
+                return closestValue;
+        }
+        
+        public static void main(String[] args) {
+                
+                TreeNode node = new TreeNode(9);
+                node.left = new TreeNode(4);
+                node.right = new TreeNode(17);
+                node.left.left = new TreeNode(3);
+                node.left.right = new TreeNode(6);
+                node.left.right.left = new TreeNode(5);
+                node.left.right.right = new TreeNode(7);
+                node.right.right = new TreeNode(22);
+                node.right.right.left = new TreeNode(20);
+                
+                System.out.println("Closest value of is = " + findClosestValue(node, 18.99));
+        }
+}
---
+++ interview/src/trees/ClosestValueInBinaryTreeII.java
@@ -0,0 +1,55 @@
+/**
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ * https://leetcode.com/problems/closest-binary-search-tree-value/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ */
+package trees;
+
+public class ClosestValueInBinaryTreeII {
+        
+        private static int findClosestValue(TreeNode root, double target) {
+                // initialize values
+                double minDiff =  Double.MAX_VALUE;
+                int closestValue = -1;
+                
+                while(root != null) {
+                        // compute current difference
+                        double currentDiff = Math.abs(target - root.data);
+                        // if current difference less then min difference so far
+                        if(currentDiff < minDiff) {
+                                minDiff = currentDiff;
+                                closestValue = root.data;
+                        }
+                        // check in left subtree
+                        if(target < root.data) {
+                                root = root.left;
+                        }
+                        // check in right subtree
+                        else if(target > root.data) {
+                                root = root.right;
+                        }
+                        // if node equals to target
+                        else {
+                                break;
+                        }
+                }
+                return closestValue;
+        }
+        
+        public static void main(String[] args) {
+                
+                TreeNode node = new TreeNode(9);
+                node.left = new TreeNode(4);
+                node.right = new TreeNode(17);
+                node.left.left = new TreeNode(3);
+                node.left.right = new TreeNode(6);
+                node.left.right.left = new TreeNode(5);
+                node.left.right.right = new TreeNode(7);
+                node.right.right = new TreeNode(22);
+                node.right.right.left = new TreeNode(20);
+                
+                System.out.println("Closest value of is = " + findClosestValue(node, 18.99));
+        }
+}
