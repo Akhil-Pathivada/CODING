f08d1ad54b9a2b35c97eef6c3006c549352d9beb
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2021-11-24 08:50:30 +0530

    added@Wed Nov 24 08:50:30 IST 2021

---
+++ interview/src/trees/InvertBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * https://leetcode.com/problems/invert-binary-tree/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(H)
+ * */
+
+package trees;
+
+public class InvertBinaryTree {
+        
+        private TreeNode invertBinaryTree(TreeNode root) {
+                // base case
+                if(root == null) {
+                        return null;
+                }
+                // store left child into temp
+                TreeNode tempLeft = root.left;
+                // make left to point to right and vice versa
+                root.left = invertBinaryTree(root.right);
+                root.right = invertBinaryTree(tempLeft);
+                
+                return root;
+        }
+        
+        public static void main(String[] args) {
+                
+                TreeNode root = new TreeNode(4);
+                root.left = new TreeNode(2);
+                root.right = new TreeNode(7);
+                root.left.left = new TreeNode(1);
+                root.left.right = new TreeNode(3);
+                root.right.left = new TreeNode(6);
+                root.right.right = new TreeNode(9);
+                
+                root.preOrder(new InvertBinaryTree().invertBinaryTree(root));
+        }
+}
