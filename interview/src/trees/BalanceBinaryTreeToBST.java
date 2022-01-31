6060a9ec9631f13e22f191aa9a716f9c21c44883
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-01-31 10:07:54 +0530

    added@Mon Jan 31 10:07:54 IST 2022

---
+++ interview/src/trees/BalanceBinaryTreeToBST.java
@@ -0,0 +1,58 @@
+/**
+ * https://leetcode.com/problems/balance-a-binary-search-tree/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ * */
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BalanceBinaryTreeToBST extends TreeNode {
+        
+        private static TreeNode sortedArrayToBST(List<TreeNode> sortedArray, int start, int end) {
+                // base case
+                if(start > end) {
+                        return null;
+                }
+                // get mid element and make as root
+                int mid = (start + end) / 2;
+                TreeNode root = sortedArray.get(mid);
+                // recur for left and right subtrees
+                root.left = sortedArrayToBST(sortedArray, start, mid - 1);
+                root.right = sortedArrayToBST(sortedArray, mid + 1, end);
+                return root;
+        }
+        
+        private static void inOrderTraverse(TreeNode root, List<TreeNode> sortedArray) {
+                // base case
+                if(root == null) {
+                        return;
+                }
+                inOrderTraverse(root.left, sortedArray);
+                // insert element into list
+                sortedArray.add(root);
+                inOrderTraverse(root.right, sortedArray);
+        }
+        
+        private static TreeNode balanceBinaryTreeToBST(TreeNode root) {
+                
+                List<TreeNode> sortedArray = new ArrayList<TreeNode>();
+                // get the tree as a sorted array
+                inOrderTraverse(root, sortedArray);
+                // convert sorted array to BST
+                return sortedArrayToBST(sortedArray, 0 , sortedArray.size() - 1);
+        }
+        
+        public static void main(String[] args) {
+                
+                TreeNode root = new TreeNode(10);
+                root.left = new TreeNode(8);
+                root.left.left = new TreeNode(7);
+                root.left.left.left = new TreeNode(6);
+                root.left.left.left.left = new TreeNode(5);
+        
+                new BalanceBinaryTreeToBST().preOrder(balanceBinaryTreeToBST(root));
+        }
+}
---
+++ interview/src/trees/SortedArrayToBST.java
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(log(N))
+ * */
+package trees;
+
+public class SortedArrayToBST extends TreeNode {
+        
+        private static TreeNode sortedArrayToBSTUtil(int[] nums, int start, int end) {
+                // base case
+                if(start > end) {
+                        return null;
+                }
+                // get mid element and make as root
+                int mid = (start + end) / 2;
+                TreeNode root = new TreeNode(nums[mid]);
+                // recur for left and right subtrees
+                root.left = sortedArrayToBSTUtil(nums, start, mid - 1);
+                root.right = sortedArrayToBSTUtil(nums, mid + 1, end);
+                return root;
+        }
+        
+        private static TreeNode sortedArrayToBST(int[] nums) {
+                // base case
+                if(nums.length == 0) {
+                        return null;
+                }
+                return sortedArrayToBSTUtil(nums, 0, nums.length - 1);
+        }
+        
+        public static void main(String[] args) {
+                
+                int nums[] = {-10, -3, 0, 5, 9};
+                new SortedArrayToBST().preOrder(sortedArrayToBST(nums));
+        }
+}
