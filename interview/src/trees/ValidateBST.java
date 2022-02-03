b7e6907a095a4529cc50514f70a96a9760c4b334
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-02-03 16:48:40 +0530

    added@Thu  3 Feb 2022 16:48:40 IST

---
+++ interview/src/trees/ValidateBST.java
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ * */
+package trees;
+
+public class ValidateBST {
+        
+        private static TreeNode prev = null;
+        
+        private static boolean isValidBST(TreeNode root) {
+                // base case
+                if(root == null) {
+                        return true;
+                }
+                // do inorder traversal
+                if(!isValidBST(root.left)) {
+                        return false;
+                }
+                if(prev != null && root.data < prev.data) {
+                        return false;
+                }
+                prev = root;
+                return isValidBST(root.right);
+        }
+        
+        public static void main(String[] args) {
+                
+                TreeNode root = new TreeNode(20);
+                root.left = new TreeNode(8);
+                root.right = new TreeNode(22);
+                root.left.left = new TreeNode(4);
+                root.left.right = new TreeNode(12);
+                root.left.right.left = new TreeNode(10);
+                root.left.right.right = new TreeNode(14);
+                
+                System.out.println(isValidBST(root));
+        }
+}
