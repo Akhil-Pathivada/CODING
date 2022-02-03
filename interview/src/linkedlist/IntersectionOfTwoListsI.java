13879448fd3aa9804d44bd917f418f0acf16e7b1
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-02-03 18:17:12 +0530

    added@Thu  3 Feb 2022 18:17:12 IST

---
+++ interview/src/linkedlist/IntersectionOfTwoListsI.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ * */
+package linkedlist;
+
+public class IntersectionOfTwoListsI {
+        
+        private static int getIntersectionNode(LinkedListNode headA, LinkedListNode headB) {
+                
+                int lenA = headA.lengthOfLinkedList(headA);
+                int lenB = headB.lengthOfLinkedList(headB);
+                // move headA and headB to the same start point
+                while(lenA > lenB) {
+                        headA = headA.next;
+                        --lenA;
+                }
+                while(lenB > lenA) {
+                        headB = headB.next;
+                        --lenB;
+                }
+                // find the intersection until end
+                while(headA != headB) {
+                        headA = headA.next;
+                        headB = headB.next;
+                }
+                return headA.data;
+        }
+        
+        public static void main(String[] args) {
+                
+                LinkedListNode headA = new LinkedListNode(10);
+                headA.next = new LinkedListNode(20);
+                headA.next.next = new LinkedListNode(30);
+                headA.next.next.next = new LinkedListNode(40);
+                headA.next.next.next.next = new LinkedListNode(50);
+        
+                LinkedListNode headB = new LinkedListNode(60);
+                headB.next = new LinkedListNode(70);
+                headB.next.next = new LinkedListNode(80);
+        
+                headB.next.next.next = headA.next.next.next;
+        
+                System.out.println(getIntersectionNode(headA, headB));
+        }
+}
---
+++ interview/src/linkedlist/IntersectionOfTwoListsII.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ * */
+package linkedlist;
+
+public class IntersectionOfTwoListsII {
+        
+        private static int getIntersectionNode(LinkedListNode headA, LinkedListNode headB) {
+        
+                LinkedListNode a = headA;
+                LinkedListNode b = headB;
+                //if a & b have different len, then we will stop the loop after second iteration
+                while(a != b) {
+                        // for the end of first iteration, we just reset the pointer to the head of another linkedlist
+                        a = (a == null) ? headB : a.next;
+                        b = (b == null) ? headA : b.next;
+                }
+                return a.data;
+        }
+        
+        public static void main(String[] args) {
+                
+                LinkedListNode headA = new LinkedListNode(10);
+                headA.next = new LinkedListNode(20);
+                headA.next.next = new LinkedListNode(30);
+                headA.next.next.next = new LinkedListNode(40);
+                headA.next.next.next.next = new LinkedListNode(50);
+                
+                LinkedListNode headB = new LinkedListNode(60);
+                headB.next = new LinkedListNode(70);
+                headB.next.next = new LinkedListNode(80);
+        
+                headB.next.next.next = headA.next.next.next;
+        
+                System.out.println(getIntersectionNode(headA, headB));
+        }
+}
