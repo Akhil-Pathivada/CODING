1b9280895467d9c9e7e5d1282ae845605c245422
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-02-04 09:09:31 +0530

    added@Fri  4 Feb 2022 09:09:31 IST

---
+++ interview/src/arrays/ContainerWithMostWaterI.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ *
+ * Time Complexity : O(N ^ 2)
+ * Space Complexity : O(1)
+ * */
+
+package arrays;
+
+public class ContainerWithMostWaterI {
+        
+        private static int maxArea(int[] height) {
+        
+                int n = height.length;
+                int maxArea = 0;
+        
+                for(int i = 0; i < n; ++i) {
+                        for(int j = i + 1; j < n; ++j) {
+                                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+                        }
+                }
+                return maxArea;
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+                System.out.println("Max. water can contains = " + maxArea(height));
+        }
+}
---
+++ interview/src/arrays/ContainerWithMostWaterII.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ * */
+
+package arrays;
+
+public class ContainerWithMostWaterII {
+        
+        private static int maxArea(int[] height) {
+                
+                int left = 0;
+                int right = height.length - 1;
+                int maxArea = 0;
+        
+                while(left < right) {
+                
+                        maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+                
+                        if(height[left] < height[right]) {
+                                ++left;
+                        }
+                        else {
+                                --right;
+                        }
+                }
+                return maxArea;
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+                System.out.println("Max. water can contains = " + maxArea(height));
+        }
+}
