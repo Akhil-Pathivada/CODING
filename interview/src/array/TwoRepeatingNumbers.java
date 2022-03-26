c8d94d75564a3d39c5e01b4a1ebbb297a333fcf8
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-03-26 21:49:40 +0530

    added@Sat Mar 26 21:49:40 IST 2022

---
+++ interview/src/array/TwoRepeatingNumbers.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * You are given an array of n+2 elements.
+ * All elements of the array are in range 1 to n.
+ * And all elements occur once except two numbers which occur twice. Find the two repeating numbers.
+ *
+ * Time Complexity ::O(N)
+ * Space Complexity : O(1)
+ * */
+package array;
+
+public class TwoRepeatingNumbers {
+        
+        private static void printTwoRepeatingNumbers(int[] arr) {
+                
+                for(int i = 0; i < arr.length; ++i) {
+                        // duplicate found
+                        if(arr[Math.abs(arr[i])] < 0) {
+                                System.out.print(Math.abs(arr[i]) + ", ");
+                        }
+                        else { // change the sign
+                                arr[Math.abs(arr[i])] = -1 * arr[Math.abs(arr[i])];
+                        }
+                }
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] arr = {1, 2, 4, 3, 1, 3};
+                
+                printTwoRepeatingNumbers(arr);
+        }
+}
---
+++ interview/src/stack/ValidParentheses.java
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ * */
+package stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+        
+        private static boolean isValid(String str) {
+        
+                Stack<Character> stack = new Stack<Character>();
+                // iterate over string
+                for(char c : str.toCharArray()) {
+                        if(c == '(') {
+                                stack.push(')');
+                        }
+                        else if(c == '{') {
+                                stack.push('}');
+                        }
+                        else if(c == '[') {
+                                stack.push(']');
+                        }
+                        else if (stack.isEmpty() || stack.pop() != c) {
+                                return false;
+                        }
+                }
+                return stack.isEmpty();
+        }
+        
+        public static void main(String[] args) {
+        
+                String str = "()[]{}";
+                System.out.println(isValid(str));
+        }
+}
