d04163c3207c77414ad5184624da315f9badb5d5
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-03-26 22:15:50 +0530

    added@Sat Mar 26 22:15:50 IST 2022

--- interview/src/array/SeparateZerosOnes.java
+++ interview/src/array/SeparateZerosOnes.java
@@ -15,13 +15,16 @@ public class SeparateZerosOnes {
                 int left = 0, right = arr.length - 1;
                 
                 while(left < right) {
-                        
+                        // Increment left index while we see 0 at left
                         while(arr[left] == 0 && left < right) {
                                 ++left;
                         }
+                        // Decrement right index while we see 1 at right
                         while(arr[right] == 1 && left < right) {
                                 --right;
                         }
+                        /** If left is smaller than right then there is a 1 at left
+                        and a 0 at right. Exchange arr[left] and arr[right] */
                         if(left < right) {
                                 // swap numbers
                                 arr[left] = 0;
