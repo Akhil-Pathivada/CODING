7236bb49b5af75555e29c029bc7cbe4151db5ec4
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-03-28 09:02:47 +0530

    added@Mon Mar 28 09:02:47 IST 2022

---
+++ interview/src/queue/LRUCacheReplacementI.java
@@ -0,0 +1,67 @@
+/**
+ * LRU Cache Implementation....
+ *
+ * Time Complexity : O(1)
+ * Space Complexity : O(N)
+ * */
+package queue;
+
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LRUCacheReplacementI {
+        
+        private final int CACHE_SIZE; // maximum capacity of cache
+        private Deque<Integer> deque; // store keys of cache
+        private HashSet<Integer> hashSet; // store references of key in cache
+        
+        private LRUCacheReplacementI(int capacity) {
+        
+                CACHE_SIZE = capacity;
+                deque = new LinkedList<>();
+                hashSet = new HashSet<>(capacity);
+        }
+        // Refer the page within the LRU cache
+        private void refer(int page) {
+                // if page not found && cache is full
+                if(!hashSet.contains(page)) {
+                        if(deque.size() == CACHE_SIZE) {
+                                // remove least recently used page: it should be last
+                                int lru = deque.removeLast();
+                                hashSet.remove(lru);
+                        }
+                }
+                else { /** The found page may not be always the last element, even if it's an intermediate
+                 element that needs to be removed and added to the start of the Queue */
+                        deque.remove(page);
+                }
+                deque.addFirst(page);
+                hashSet.add(page);
+        }
+        // display contents of cache
+        private void displayCache() {
+                
+                Iterator<Integer> itr = deque.iterator();
+                while (itr.hasNext()) {
+                        System.out.print(itr.next() + " ");
+                }
+        }
+        public static void main(String[] args) {
+                
+                LRUCacheReplacementI cache = new LRUCacheReplacementI(4);
+        
+                cache.refer(1);
+                cache.refer(2);
+                cache.refer(3);
+                cache.refer(1);
+                cache.refer(4);
+                cache.refer(5);
+                cache.refer(2);
+                cache.refer(2);
+                cache.refer(1);
+        
+                cache.displayCache();
+        }
+}
---
+++ interview/src/queue/LRUCacheReplacementII.java
@@ -0,0 +1,78 @@
+/**
+ * LRU Cache Implementation....
+ *
+ * (Using LinkedHashSet)
+ *
+ * Time Complexity : O(1)
+ * Space Complexity : O(N)
+ * */
+package queue;
+
+import java.util.*;
+
+public class LRUCacheReplacementII {
+        
+        private final int CACHE_SIZE; // maximum capacity of cache
+        private Set<Integer> cache; // store references of key in cache
+        
+        private LRUCacheReplacementII(int capacity) {
+                
+                CACHE_SIZE = capacity;
+                cache = new LinkedHashSet<>(capacity);
+        }
+        /** This function returns false if key is not present in cache. Else it moves the key to
+          front by first removing it and then adding it, and returns true. */
+        private boolean get(int page) {
+                if(!cache.contains(page)) {
+                        return false;
+                }
+                cache.remove(page);
+                cache.add(page);
+                return true;
+        }
+        
+        /** if cache is full, remove first element in cache(which is lru)
+          and insert new page into cache */
+        private void put(int page) {
+                if(cache.size() == CACHE_SIZE) {
+                        int firstKey = cache.iterator().next();
+                        cache.remove(firstKey);
+                }
+                cache.add(page);
+        }
+        
+        // Refer the page within the LRU cache && insert if page not found
+        private void refer(int page) {
+                if(!get(page)) {
+                        put(page);
+                }
+        }
+        // display contents of cache
+        private void displayCache() {
+        
+                LinkedList<Integer> list = new LinkedList<>(cache);
+        
+                /** The descendingIterator() method of java.util.LinkedList class is used to return an iterator over the elements
+                 in this LinkedList in reverse sequential order */
+                Iterator<Integer> itr = list.descendingIterator();
+        
+                while (itr.hasNext())
+                        System.out.print(itr.next() + " ");
+        }
+        public static void main(String[] args) {
+                
+                LRUCacheReplacementII ca = new LRUCacheReplacementII(4);
+                
+                ca.refer(1);
+                ca.refer(2);
+                ca.refer(3);
+                ca.refer(1);
+                ca.refer(4);
+                ca.refer(5);
+                ca.refer(2);
+                ca.refer(2);
+                ca.refer(1);
+                
+                ca.displayCache();
+        }
+}
