646b3e1a1bc44e7978d97d5abafcd84c769d3574
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-02 06:42:43 +0530

    added@Sat Apr  2 06:42:43 IST 2022

---
+++ interview/src/slidingwindow/LongestSubstringWithoutRepeatingCharactersI.java
@@ -0,0 +1,57 @@
+/**
+ * Date 02/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ *
+ */
+package slidingwindow;
+
+import java.util.HashSet;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharactersI {
+        
+        private static boolean isDistinct(String s, int i, int j) {
+        
+                // Note : Default values in visited are false
+                boolean[] visited = new boolean[26];
+                HashSet<Character> set = new HashSet<>();
+        
+                for(int k = i; k <= j; k++) {
+                        
+                        if (set.contains(s.charAt(k))) {
+                                return false;
+                        }
+                        set.add(s.charAt(k));
+                }
+                return true;
+        }
+        
+        private static int longestSubstring(String s) {
+                
+                HashSet<Character> set = new HashSet<>();
+                int n = s.length(), maxLen = 0;
+                
+                for(int i =0; i < n; ++i) {
+                        
+                        for(int j = i; j < n; ++j) {
+                                
+                                if(isDistinct(s, i , j)) {
+                                        maxLen = Math.max(j - i + 1, maxLen);
+                                }
+                        }
+                }
+                return maxLen;
+        }
+        
+        public static void main(String[] args) {
+                
+                String str = "abcabcbb";
+                System.out.printf("Longest Substring without repeating characters = %d ", longestSubstring(str));
+        }
+}
---
+++ interview/src/slidingwindow/LongestSubstringWithoutRepeatingCharactersII.java
@@ -0,0 +1,43 @@
+/**
+ * Date 02/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Time Complexity : O(N ^ 3)
+ * Space Complexity : O(1)
+ *
+ */
+package slidingwindow;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharactersII {
+        
+        private static int longestSubstring(String s) {
+                
+                HashSet<Character> set = new HashSet<>();
+                int i = 0, j = 0, maxLength = 0;
+                
+                while (j < s.length()) {
+                        // first time visiting a character : so include it in max length
+                        if(!set.contains(s.charAt(j))) {
+                                set.add(s.charAt(j));
+                                maxLength = Math.max(set.size(), maxLength);
+                                ++j;
+                        }
+                        else { // already visited the character : so slide the window
+                                set.remove(s.charAt(i));
+                                ++i;
+                        }
+                }
+                return maxLength;
+        }
+        
+        public static void main(String[] args) {
+                
+                String str = "abcabcbb";
+                System.out.printf("Longest Substring without repeating characters = %d ", longestSubstring(str));
+        }
+}
