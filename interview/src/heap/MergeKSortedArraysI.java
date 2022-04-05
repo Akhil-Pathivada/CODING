454dd8ef965892c979a3d9ecb153f11f515d6698
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-05 09:55:59 +0530

    added@Tue Apr  5 09:55:59 IST 2022

---
+++ interview/src/heap/MergeKSortedArraysI.java
@@ -0,0 +1,44 @@
+/**
+ * Date 05/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://www.geeksforgeeks.org/merge-k-sorted-arrays/
+ *
+ * Time Complexity : O((K * N) * log(K * N))
+ * Space Complexity : O(K * N)
+ *
+ */
+package heap;
+
+import java.util.Arrays;
+
+public class MergeKSortedArraysI {
+        
+        private static void mergeArrays(int[][] arr, int[] output, int k, int n) {
+                
+                for(int i = 0, c = 0; i < k; ++i) {
+                        
+                        for(int j = 0; j < n; ++j) {
+                                
+                                output[c++] = arr[i][j];
+                        }
+                }
+               Arrays.sort(output);
+        }
+        
+        public static void main(String[] args) {
+                
+                int[][] arr = {
+                        { 2, 6, 12, 34 },
+                        { 1, 9, 20, 1000 },
+                        { 23, 34, 90, 2000 }
+                };
+                int k = 3; // k arrays
+                int n = 4; // length of each array
+                int[] output = new int[k * n];
+                
+                mergeArrays(arr, output, k, n);
+                System.out.println(Arrays.toString(output));
+        }
+}
