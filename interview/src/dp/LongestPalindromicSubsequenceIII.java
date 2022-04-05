a11cdcf3ecc0ccfe5f1b20a082df81925b3851ec
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-05 11:27:13 +0530

    added@Tue Apr  5 11:27:13 IST 2022

---
+++ interview/src/dp/LongestPalindromicSubsequenceIII.java
@@ -0,0 +1,50 @@
+/**
+ * Date 05/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/longest-palindromic-subsequence/
+ *
+ * Here, we use LCS to find the LPS
+ *
+ * Time Complexity : O(N ^ 2)
+ * Space Complexity : O(N ^ 2)
+ *
+ */
+package dp;
+
+import com.sun.javafx.binding.StringFormatter;
+import java.util.*;
+
+public class LongestPalindromicSubsequenceIII {
+        
+        private static int LCS(char[] X, char[] Y, int m, int n) {
+                
+                int table[][] = new int[m + 1][n + 1];
+                
+                for(int i = 0; i <= m; ++i) {
+                        
+                        for(int j = 0; j <= n; ++j) {
+                                
+                                if(i == 0 || j == 0) {
+                                        table[i][j] = 0;
+                                }
+                                else if(X[i - 1] == Y[j - 1]) {
+                                        table[i][j] = 1 + table[i - 1][j - 1];
+                                }
+                                else {
+                                        table[i][j] = Math.max(table[i - 1][j], table[i][j - 1]);
+                                }
+                        }
+                }
+                return table[m][n];
+        }
+        
+        public static void main(String[] args) {
+                
+                String X = "GEEKSFORGEEKS"; // input
+                
+                String Y = new StringBuilder(X).reverse().toString();
+                System.out.printf("The length of the LPS is = %d ", LCS(X.toCharArray(), Y.toCharArray(), X.length(), Y.length()));
+        }
+}
