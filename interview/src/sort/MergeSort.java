502600f6a93c5183950b22d1cb7d23117bbecd5d
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-06 08:02:24 +0530

    added@Wed Apr  6 08:02:24 IST 2022

---
+++ interview/src/sort/MergeSort.java
@@ -0,0 +1,69 @@
+/**
+ * Date 06/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * Time Complexity : O(N * log(N))
+ * Space Complexity : O(N)
+ */
+package sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+        // Merges two subarrays of arr[]. First subarray is arr[p..q] Second subarray is arr[q+1..r]
+        private static void merge(int[] arr, int p, int q, int r) {
+                
+                int i, j;
+                // find sizes of two subarrays to be merged
+                int n1 = q - p + 1;
+                int n2 = r - q;
+                // create temp arrays
+                int L[] = new int[n1 + 1];
+                int R[] = new int[n2 + 1];
+                // copy data to temp arrays L[] and R[]
+                for(i = 0; i < n1; ++i) {
+                        L[i] = arr[p + i];
+                }
+                for(j = 0; j < n2; ++j) {
+                        R[j] = arr[q + 1 + j];
+                }
+                L[n1] = R[n2] = Integer.MAX_VALUE;
+                // initial indexes of first and second subarrays
+                i = j = 0;
+                // Merge the temp arrays back into arr[p..r]
+                for(int k = p; k <= r; ++k) {
+                        
+                        if(L[i] <= R[j]) {
+                                arr[k] = L[i++];
+                        }
+                        else {
+                                arr[k] = R[j++];
+                        }
+                }
+        }
+        
+        private static void sort(int[] arr, int p, int r) {
+                
+                if(p < r) {
+                        
+                        int q = (p + r) / 2;
+                        // call sort on first and second halves
+                        sort(arr, p, q);
+                        sort(arr, q + 1, r);
+                        // merge first and second halves
+                        merge(arr, p, q, r);
+                }
+        }
+        
+        public static void main(String args[]) {
+                
+                int arr[] = { 12, 11, 13, 5, 6, 7 };
+                
+                System.out.println("Given Array" + Arrays.toString(arr));
+                
+                sort(arr, 0, arr.length - 1);
+                
+                System.out.println("Sorted array" +  Arrays.toString(arr));
+        }
+}
---
+++ interview/src/sort/QuickSort.java
@@ -0,0 +1,66 @@
+/**
+ * Date 06/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * Best Case :
+ *      Time Complexity : N * log(N)
+ *      Space Complexity : log(N)
+ *
+ * Worst Case :
+ *      Time Complexity : O(N ^ 2)
+ *      Space Complexity : O(N)
+ */
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+        
+        private static void swap(int[] arr, int i, int j) {
+                
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+        }
+        /** This function takes last element as pivot, places
+        the pivot element at its correct position in sorted
+        array, and places all smaller (smaller than pivot)
+        to left of pivot and all greater elements to right of pivot */
+        private static int partition(int[] arr, int low, int high) {
+        
+                int x = arr[high];
+                int i = low - 1;
+                
+                for(int j = low; j < high; ++j) {
+                        // if current element is smaller than the pivot
+                        if(arr[j] <= x) {
+                                swap(arr, ++i, j);
+                        }
+                }
+                swap(arr, ++i, high);
+                return i;
+        }
+        
+        private static void sort(int[] arr, int low, int high) {
+                
+                if(low < high) {
+                        // pivot is partitioning index, arr[p] is now at right place
+                        int pivot = partition(arr, low, high);
+                        // separately sort elements before partition and after partition
+                        sort(arr, low, pivot - 1);
+                        sort(arr, pivot + 1, high);
+                }
+        }
+        
+        public static void main(String args[]) {
+                
+                int arr[] = { 12, 11, 13, 5, 6, 7 };
+                
+                System.out.println("Given Array" + Arrays.toString(arr));
+                
+                sort(arr, 0, arr.length - 1);
+                
+                System.out.println("Sorted array" +  Arrays.toString(arr));
+        }
+}
