1c474293e4d38f7e9aa2b6ceb2a412ab9b866d81
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-08 11:36:24 +0530

    added@Fri Apr  8 11:36:24 IST 2022

---
+++ interview/src/dp/LongestBitonicSubsequence.java
@@ -0,0 +1,66 @@
+/**
+ * Date 08/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://www.geeksforgeeks.org/longest-bitonic-subsequence-dp-15/
+ *
+ * Time Complexity : O(N ^ 2)
+ * Space Complexity : O(N)
+ */
+package dp;
+
+public class LongestBitonicSubsequence {
+        
+        private static int LBS(int[] nums, int n) {
+        
+                // store longest increasing sequence till that point
+                int lis[] = new int[n];
+                // store longest decreasing sequence till that point
+                int[] lds = new int[n];
+                // compute LIS
+                lis[0] = 1;
+                for(int i = 1; i < n; ++i) {
+                
+                        lis[i] = 1;
+                        // iterate over previous elements and check adding current element
+                        // to its sequence can increase result
+                        for (int j = 0; j < i; ++j) {
+                        
+                                if(nums[i] > nums[j] && lis[j] + 1 > lis[i]) {
+                                        lis[i] = lis[j] + 1;
+                                }
+                        }
+                }
+                // compute LDS
+                lds[n - 1] = 1;
+                for(int i = n - 2; i >= 0; --i) {
+                
+                        lds[i] = 1;
+                        // iterate over next elements and check adding current element
+                        // to its sequence can increase result
+                        for(int j = n - 1; j > i; --j) {
+                        
+                                if(nums[i] > nums[j] && lds[j] + 1 > lds[i]) {
+                                        lds[i] = lds[j] + 1;
+                                }
+                        }
+                }
+                
+                
+                // compute and get the maximum by using both lis[] and lds[]
+                int maxLength = lis[0] + lds[0] - 1;
+                for(int i = 1; i < n; ++i) {
+                        if(lis[i] + lds[i] - 1 > maxLength) {
+                                maxLength = lis[i] + lds[i] - 1;
+                        }
+                }
+                return maxLength;
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] nums = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+                System.out.printf("Length of LBS is = %d ", LBS(nums, nums.length));
+        }
+}
--- interview/src/dp/LongestDecreasingSubsequence.java
+++ interview/src/dp/LongestDecreasingSubsequence.java
@@ -3,35 +3,35 @@
  *
  * @author akhilpathivada
  *
- * https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/
+ * https://www.geeksforgeeks.org/longest-decreasing-subsequence/
  *
  * Time Complexity : O(N ^ 2)
  * Space Complexity : O(N)
  */
 package dp;
 
-public class LongestIncreasingSubsequence {
+public class LongestDecreasingSubsequence {
         
-        private static int LIS(int[] nums, int n) {
-                // store longest increasing sequence till that point
-                int lis[] = new int[n];
-                lis[0] = 1;
+        private static int LDS(int[] nums, int n) {
+                // store longest decreasing sequence till that point
+                int[] lds = new int[n];
+                lds[0] = 1;
                 
                 for(int i = 1; i < n; ++i) {
                         
-                        lis[i] = 1;
+                        lds[i] = 1;
                         // iterate over previous elements and check adding current element
                         // to its sequence can increase result
-                        for (int j = 0; j < i; ++j) {
+                        for(int j = 0; j < i; ++j) {
                                 
-                                if(nums[i] > nums[j] && lis[j] + 1 > lis[i]) {
-                                        lis[i] = lis[j] + 1;
+                                if(nums[i] < nums[j] && lds[j] + 1 > lds[i]) {
+                                        lds[i] = lds[j] + 1;
                                 }
                         }
                 }
                 // get the maximum from lis[]
                 int maxLength = Integer.MIN_VALUE;
-                for(int i : lis) {
+                for(int i : lds) {
                         if(i > maxLength) {
                                 maxLength = i;
                         }
@@ -41,7 +41,7 @@ public class LongestIncreasingSubsequence {
         
         public static void main(String[] args) {
                 
-                int[] nums = { 10, 22, 9, 33, 21, 50, 41, 60 };
-                System.out.printf("Longest Increasing Subsequence : %d ", LIS(nums, nums.length));
+                int[] nums = { 15, 27, 14, 38, 63, 55, 46, 65, 85 };
+                System.out.printf("Length of LDS is = %d ", LDS(nums, nums.length));
         }
 }
