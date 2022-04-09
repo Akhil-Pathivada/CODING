b297d7ff78ee2f1cf100ce8dbe0349d7e4bca705
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-09 18:28:18 +0530

    added@Sat Apr  9 18:28:18 IST 2022

---
+++ interview/src/binarysearch/SearchInsertPosition.java
@@ -0,0 +1,45 @@
+/**
+ * Date 09/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/search-insert-position/
+ *
+ * Time Complexity : O(log(N))
+ * Space Complexity : O(1)
+ */
+package binarysearch;
+
+public class SearchInsertPosition {
+        
+        private static int searchInsert(int[] nums, int target) {
+                
+                int start = 0;
+                int end = nums.length - 1;
+                
+                while(start <= end) {
+                
+                        int mid = (start + end) / 2;
+                        if( mid > 0 && nums[mid] >= target && nums[mid - 1] < target) {
+                                return mid;
+                        }
+                        else if(mid == 0 && nums[mid] >= target) {
+                                return mid;
+                        }
+                        if(nums[mid] < target) {
+                                start = mid + 1;
+                        }
+                        else {
+                                end = mid;
+                        }
+                }
+                return nums.length;
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] nums = { 1, 3, 5, 6};
+                int target = 7;
+                System.out.println(searchInsert(nums, target));
+        }
+}
