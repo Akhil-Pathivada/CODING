cadea549ea40a6c5ab8ae89e85207ff7910c8484
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-09 18:06:57 +0530

    added@Sat Apr  9 18:06:57 IST 2022

---
+++ interview/src/dp/HouseRobber.java
@@ -0,0 +1,42 @@
+/**
+ * Date 09/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/house-robber/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ */
+package dp;
+
+public class HouseRobber {
+        
+        private static int rob(int[] nums, int n) {
+
+                if(n == 0) {
+                        return 0;
+                }
+                if(n == 1) {
+                        return nums[0];
+                }
+                if(n == 2) {
+                        return Math.max(nums[0], nums[1]);
+                }
+        
+                int[] dp = new int[n];
+                dp[0] = nums[0];
+                dp[1] = Math.max(nums[0], nums[1]);
+                
+                for(int i = 2; i < n; ++i) {
+                        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+                }
+                return dp[n - 1];
+        }
+        
+        public static void main(String[] args) {
+                
+                int[] nums = { 2, 7, 9, 3, 1};
+                System.out.println("Max amount you can rob = " + rob(nums, nums.length));
+        }
+}
