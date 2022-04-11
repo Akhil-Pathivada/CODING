89268cf7c1b2847fe139afbacc6814397ca39850
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-11 06:57:43 +0530

    added@Mon Apr 11 06:57:43 IST 2022

---
+++ interview/src/stack/LargestRectangleInHistogram.java
@@ -0,0 +1,59 @@
+/**
+ * Date 11/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/
+ *
+ *
+ */
+package stack;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+        
+        private static int largestRectangleArea(int[] heights, int n) {
+                
+                // Create an empty stack. The stack holds indexes of heights[] array
+                // The bars stored in stack are always in increasing order of their heights.
+                Stack<Integer> stack = new Stack<>();
+                
+                int top, area, maxArea = 0;
+                // Run through all bars of given histogram
+                int i = 0;
+                while(i < n) {
+                        // If this bar is higher than the bar on top stack, push it to stack
+                        if(stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+                                stack.push(i++);
+                        }
+                        // If this bar is lower than top of stack, then calculate area of rectangle
+                        // with stack top as the smallest (or minimum height) bar. 'i' is
+                        // 'right index' for the top and element before top in stack is 'left index'
+                        else {
+                                top = stack.pop();
+                                // Calculate the area with heights[top] stack as smallest bar
+                                area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+                                // update max area, if needed
+                                maxArea = Math.max(maxArea, area);
+                        }
+                }
+                // Now pop the remaining bars from stack and calculate area with every
+                // popped bar as the smallest bar
+                while(!stack.isEmpty()) {
+                        
+                        top = stack.pop();
+                        // Calculate the area with heights[top] stack as smallest bar
+                        area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+                        // update max area, if needed
+                        maxArea = Math.max(maxArea, area);
+                }
+                return maxArea;
+        }
+        
+        public static void main(String[] args) {
+        
+                int heights[] = { 6, 2, 5, 4, 5, 1, 6 };
+                System.out.println("Maximum area is = " + largestRectangleArea(heights, heights.length));
+        }
+}
