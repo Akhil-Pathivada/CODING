9fe7cf39f0ba505f0c5ad2f0a5ef4bc41bf21e62
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-11 17:07:55 +0530

    added@Mon Apr 11 17:07:55 IST 2022

---
+++ interview/src/array/MaximumLengthOfSubarrayWithEqualZerosOnes.java
@@ -0,0 +1,51 @@
+/**
+ * Date 11/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/contiguous-array/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ */
+package array;
+
+import java.util.HashMap;
+
+public class MaximumLengthOfSubarrayWithEqualZerosOnes {
+        
+        private static int findMaxLength(int[] nums, int n) {
+                
+                // make all 0s to -1
+                for(int i = 0; i < n; ++i) {
+                        if(nums[i] == 0) {
+                                nums[i] = -1;
+                        }
+                }
+                
+                int sum = 0, max = 0;
+                // store the presum
+                HashMap<Integer, Integer> map = new HashMap<>();
+                map.put(0, -1);
+                
+                for(int i = 0; i < n; ++i) {
+                        
+                        sum += nums[i];
+                        // found, update the length
+                        if(map.containsKey(sum)) {
+                                max = Math.max(i - map.get(sum), max);
+                        }
+                        else {
+                                map.put(sum, i);
+                        }
+                }
+                return max;
+        }
+        
+        public static void main(String[] args) {
+        
+                int[] nums = { 0, 1, 0, 0, 1, 1, 0};
+        
+                System.out.println("Length of largest subarray with equal 0s 1s = " + findMaxLength(nums, nums.length));
+        }
+}
