c30e1c6de902c4eb20d6566af55b959ff4d59cbb
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-11 15:24:42 +0530

    added@Mon Apr 11 15:24:42 IST 2022

---
+++ interview/src/dp/IsSubsequenceI.java
@@ -0,0 +1,34 @@
+/**
+ * Date 11/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/is-subsequence/
+ *
+ * Time Complexity : O(2 ^ (m))
+ * Space Complexity : O(m)
+ */
+package dp;
+
+public class IsSubsequenceI {
+        
+        private static boolean isSubsequence(char[] s, char[] t, int m, int n) {
+                
+                if(m == 0) {
+                        return true;
+                }
+                if(n == 0) {
+                        return false;
+                }
+                if(s[m - 1] == t[n - 1]) {
+                        return isSubsequence(s, t, m - 1, n - 1);
+                }
+                return isSubsequence(s, t, m, n - 1);
+        }
+        
+        public static void main(String[] args) {
+        
+                String s ="abc", t = "ahbgdc";
+                System.out.println(isSubsequence(s.toCharArray(), t.toCharArray(), s.length(), t.length()));
+        }
+}
---
+++ interview/src/dp/IsSubsequenceII.java
@@ -0,0 +1,42 @@
+/**
+ * Date 11/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/is-subsequence/
+ *
+ * Time Complexity : O(m)
+ * Space Complexity : O(1)
+ */
+package dp;
+
+public class IsSubsequenceII {
+        
+        private static boolean isSubsequence(char[] s, char[] t, int m, int n) {
+        
+                if(m == 0) {
+                        return true;
+                }
+        
+                int indexS = 0, indexT = 0;
+                
+                while(indexT < n) {
+                
+                        if(s[indexS] == t[indexT]){
+                        
+                                ++indexS;
+                                if(indexS == m) {
+                                        return true;
+                                }
+                        }
+                        ++indexT;
+                }
+                return false;
+        }
+        
+        public static void main(String[] args) {
+        
+                String s ="abc", t = "ahbgdc";
+                System.out.println(isSubsequence(s.toCharArray(), t.toCharArray(), s.length(), t.length()));
+        }
+}
