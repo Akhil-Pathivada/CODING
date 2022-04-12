e50a887acf71899bd9a1a1365406519e60e74827
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-12 19:10:19 +0530

    added@Tue Apr 12 19:10:19 IST 2022

---
+++ interview/src/dp/MinimumOperationsToReachNFrom0.java
@@ -0,0 +1,45 @@
+/**
+ * Date 12/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://practice.geeksforgeeks.org/problems/find-optimum-operation4504/1
+ *
+ *
+ */
+package dp;
+
+public class MinimumOperationsToReachNFrom0 {
+        
+        private static int minOperations(int n) {
+                
+                if(n == 0) {
+                        return 0;
+                }
+                if(n == 1) {
+                        return 1;
+                }
+                if(n == 2) {
+                        return 2;
+                }
+                int[] dp = new int[n + 1];
+                dp[0] = 0; dp[1] = 1; dp[2] = 2;
+        
+                for(int i = 3; i <= n; ++i) {
+                
+                        if(i % 2 == 0) {
+                                dp[i] = Math.min(dp[i/2], dp[i - 1]) + 1;
+                        }
+                        else {
+                                dp[i] = dp[i - 1] + 1;
+                        }
+                }
+                return dp[n];
+        }
+        
+        public static void main(String[] args) {
+                
+                int n = 8;
+                System.out.println(minOperations(n));
+        }
+}
