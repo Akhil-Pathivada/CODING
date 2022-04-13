c4b9bd506ddf3f92cc817b62c1db292dae3c7e3e
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-13 08:08:22 +0530

    added@Wed Apr 13 08:08:22 IST 2022

---
+++ interview/src/stack/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,49 @@
+/**
+ * Date 13/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(N)
+ */
+package stack;
+
+import java.util.Stack;
+
+public class MinimumRemoveToMakeValidParentheses {
+        
+        private String minRemove(String s) {
+                StringBuilder sb = new StringBuilder(s);
+                Stack<Integer> stack = new Stack<>();
+                for(int i = 0; i < sb.length(); ++i) {
+                        // push index of '('
+                        if(sb.charAt(i) == '(') {
+                                stack.push(i);
+                        }
+                        else if (sb.charAt(i) == ')'){
+                                // balanced
+                                if(!stack.isEmpty()) {
+                                        stack.pop();
+                                }
+                                // replace ')' with '*'
+                                else {
+                                        sb.setCharAt(i, '*');
+                                }
+                        }
+                }
+                // replace ')' with '*'
+                while(!stack.isEmpty()) {
+                        sb.setCharAt(stack.pop(), '*');
+                }
+                // repalce '*' with ''
+                return sb.toString().replaceAll("\\*", "");
+        }
+        
+        public static void main(String[] args) {
+                
+                String s = "lee(t(c)o)de)";
+                System.out.println("Valid string after removal = " + new MinimumRemoveToMakeValidParentheses().minRemove(s));
+        }
+}
