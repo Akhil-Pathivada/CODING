6377b625d6315e24ede4aa4588eb5c5a94ad9eda
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-19 16:46:05 +0530

    added@Tue Apr 19 16:46:05 IST 2022

---
+++ interview/src/array/MissingElement.java
@@ -0,0 +1,31 @@
+/**
+ * Date 19/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * http://www.ardendertat.com/2011/09/27/programming-interview-questions-4-find-missing-element/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ */
+package array;
+
+public class MissingElement {
+        
+        private int find(int[] arr1, int[] arr2) {
+                int result = 0;
+                for(int num : arr1) {
+                        result ^= num;
+                }
+                for(int num : arr2) {
+                        result ^= num;
+                }
+                return result;
+        }
+        
+        public static void main(String[] args) {
+                int[] arr1 = { 4, 1, 0, 2, 9, 6, 8, 7, 5, 3 };
+                int[] arr2 = { 6, 4, 7, 2, 1, 0, 8, 3, 9 };
+                System.out.printf("Missing element = %d ", new MissingElement().find(arr1, arr2));
+        }
+}
---
+++ interview/src/misc/OddOccurance.java
@@ -0,0 +1,27 @@
+/**
+ * Date 19/04/2022
+ *
+ * @author akhilpathivada
+ *
+ * https://www.geeksforgeeks.org/find-the-number-occurring-odd-number-of-times/
+ *
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ */
+package misc;
+
+public class OddOccurance {
+        
+        private int oddOccurance(int[] nums) {
+                int result = 0;
+                for(int num : nums) {
+                        result ^= num;
+                }
+                return result;
+        }
+        
+        public static void main(String[] args) {
+                int[] nums = { 2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2 };
+                System.out.println(new OddOccurance().oddOccurance(nums));
+        }
+}
