af0b68301e8e84767887f6624a23072864e5d4f4
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-04-25 17:47:08 +0530

    added@Mon Apr 25 17:47:08 IST 2022

---
+++ interview/src/dp/MinCostPath.java
@@ -0,0 +1,37 @@
+/**
+ * Date 25/04/2022
+ *
+ * @author akhilpathivada
+ */
+package dp;
+
+public class MinCostPath {
+        
+        private int minCost(int[][] cost, int m, int n) {
+                int[][] mc = new int[m][n];
+                int i, j;
+                mc[0][0] = cost[0][0];
+                for (i = 1; i < m; ++i) {
+                        mc[i][0] = cost[i - 1][0] + cost[i][0];
+                }
+                for (j = 1; j < n; ++j) {
+                        mc[0][j] = cost[0][j - 1] + cost[0][j];
+                }
+                for (i = 1; i < m; ++i) {
+                        for (j = 1; j < n; ++j) {
+                                mc[i][j] = Math.min(mc[i - 1][j - 1],
+                                        Math.min(mc[i - 1][j], mc[i][j - 1])) + cost[i][j];
+                        }
+                }
+                return mc[m - 1][n - 1];
+        }
+        
+        public static void main(String[] args) {
+                int cost[][]= {
+                        { 1, 2, 3 },
+                        { 4, 8, 2 },
+                        { 1, 5, 3 }
+                };
+                System.out.println(new MinCostPath().minCost(cost, 2, 2));
+        }
+}
