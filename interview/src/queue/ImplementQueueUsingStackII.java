cb94f5c9cb1341f115e97a4deed3f156a6cea729
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-05-05 21:18:56 +0530

    added@Thu May  5 21:18:56 IST 2022

---
+++ interview/src/queue/ImplementQueueUsingStackII.java
@@ -0,0 +1,54 @@
+/**
+ * Date 05/05/2022
+ *
+ * @author akhilpathivada
+ *
+ * Using Two Stacks
+ *
+ * Time Complexity : For Push : O(1), Pop : O(1) -> Amortised
+ * Space Complexity : O(N)
+ */
+package queue;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStackII {
+        // Implement a queue using two stacks
+        static class Queue<T> {
+                Stack<T> input, output;
+                Queue() {
+                        input = new Stack<>();
+                        output = new Stack<>();
+                }
+                void enqueue(T data) {
+                        // push item into the input stack
+                        input.push(data);
+                }
+                // Remove an item from the stack
+                T dequeue() {
+                        // if the output stack is empty
+                        // push elements from input stack to output
+                        if (output.isEmpty()) {
+                                while (!input.isEmpty()) {
+                                        output.push(input.peek());
+                                        input.pop();
+                                }
+                        }
+                        // return the top item from the output stack
+                        T top = output.peek();
+                        output.pop();
+                        return top;
+                }
+        }
+        
+        public static void main(String[] args) {
+                int[] keys = { 1, 2, 3, 4, 5 };
+                Queue<Integer> queue = new Queue<Integer>();
+                // insert above keys
+                for (int key: keys) {
+                        queue.enqueue(key);
+                }
+                System.out.println(queue.dequeue());    // print 1
+                System.out.println(queue.dequeue());
+        }
+}
