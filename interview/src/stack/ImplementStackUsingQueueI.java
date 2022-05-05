913db8415378602f2400965aa8101f44afeb010b
Akhil-Pathivada <akhil.pathivada@freshworks.com>
2022-05-05 19:45:21 +0530

    added@Thu May  5 19:45:21 IST 2022

---
+++ interview/src/stack/ImplementStackUsingQueueI.java
@@ -0,0 +1,66 @@
+/**
+ * Date 05/05/2022
+ *
+ * @author akhilpathivada
+ *
+ * Using Two Queues
+ *
+ * Time Complexity for Push : O(N), Pop : O(1)
+ * Space Complexity : O(N)
+ */
+package stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueueI {
+        // Implement stack using two queues
+        static class Stack<T> {
+                Queue<T> q1, q2;
+                // Constructor
+                Stack() {
+                        q1 = new LinkedList<>();
+                        q2 = new LinkedList<>();
+                }
+                // Push an item into the stack
+                void push(T data) {
+                        // Move all elements from the first queue to the second queue
+                        while (!q1.isEmpty()) {
+                                q2.add(q1.peek());
+                                q1.poll();
+                        }
+                        // Push the given item into the first queue
+                        q1.add(data);
+                        // Move all elements back to the first queue from the second queue
+                        while (!q2.isEmpty()) {
+                                q1.add(q2.peek());
+                                q2.poll();
+                        }
+                }
+                // Remove the top item from the stack
+                T pop() {
+                        // if the first queue is empty
+                        if (q1.isEmpty()) {
+                                System.out.println("Underflow!!");
+                                System.exit(0);
+                        }
+                        // return the front item from the first queue
+                        T front = q1.peek();
+                        q1.poll();
+                        return front;
+                }
+        }
+        
+        public static void main(String[] args) {
+                int[] keys = { 1, 2, 3, 4, 5 };
+                // insert the above keys into the stack
+                Stack<Integer> s = new Stack<Integer>();
+                for (int key: keys) {
+                        s.push(key);
+                }
+                // printing elements of stack
+                for (int i = 0; i <= keys.length; i++) {
+                        System.out.println(s.pop());
+                }
+        }
+}
---
+++ interview/src/stack/ImplementStackUsingQueueII.java
@@ -0,0 +1,61 @@
+/**
+ * Date 05/05/2022
+ *
+ * @author akhilpathivada
+ *
+ * Using One Queue
+ *
+ * Time Complexity for Push : O(N), Pop : O(1)
+ * Space Complexity : O(N)
+ */
+package stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueueII {
+        // Implement stack using two queues
+        static class Stack<T> {
+                Queue<T> queue;
+                // Constructor
+                Stack() {
+                        queue = new LinkedList<>();
+                }
+                // Push an item into the stack
+                void push(T data) {
+                        // get previous size of queue
+                        int size = queue.size();
+                        // Add current element
+                        queue.add(data);
+                        for (int i = 0; i < size; ++i) {
+                                // this will add front element into rear of queue
+                                T front = queue.remove();
+                                queue.add(front);
+                        }
+                }
+                // Remove the top item from the stack
+                T pop() {
+                        // if the first queue is empty
+                        if (queue.isEmpty()) {
+                                System.out.println("Underflow!!");
+                                System.exit(0);
+                        }
+                        T front = queue.peek();
+                        queue.poll();
+                        return front;
+                }
+        }
+        
+        public static void main(String[] args) {
+                int[] keys = { 1, 2, 3, 4, 5 };
+                // insert the above keys into the stack
+                Stack<Integer> s = new Stack<Integer>();
+                for (int key: keys) {
+                        s.push(key);
+                }
+                // printing elements of stack
+                for (int i = 0; i <= keys.length; i++) {
+                        System.out.println(s.pop());
+                }
+        }
+}
